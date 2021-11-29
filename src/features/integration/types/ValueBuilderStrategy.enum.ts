5af8c09521fb3bac610ecb1db7bbef792ddf476a
ingrie <ingride89@gmail.com>
2021-11-29 09:18:40 +0100

    FST 83 remove duplicate code

--- src/features/integration/types/ValueBuilderStrategy.enum.ts
+++ src/features/integration/types/ValueBuilderStrategy.enum.ts
@@ -1,14 +1,3 @@
-export interface property {
-    order: number;
-    key: string;
-    source: string;
-}
-
-export interface propertyString {
-    value?: string;
-    properties?: property[]
-}
-
 export enum VALUE_BUILDER_STRATEGY {
     FIXED_ARCHIVE_CODE_VALUE,
     COMBINE_STRING_VALUE
