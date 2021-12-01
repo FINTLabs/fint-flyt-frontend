071d4677ac47dbab5dc6b184659ad604ee30542e
ingrie <ingride89@gmail.com>
2021-12-01 12:36:07 +0100

    FST 84 pull 83

--- src/features/integration/components/form/ApplicantForm.tsx
+++ src/features/integration/components/form/ApplicantForm.tsx
@@ -7,12 +7,12 @@ import {IInputField} from "../../types/InputField";
 
 const ApplicantForm: React.FunctionComponent<any> = (props) => {
     const applicantFormFields: IInputField[] = [
-        {input: INPUT_TYPE.TEXT_FIELD, label: "Navn", formValue: "applicantData.name"},
-        {input: INPUT_TYPE.TEXT_FIELD, label: "Adresse", formValue: "applicantData.address"},
-        {input: INPUT_TYPE.TEXT_FIELD, label: "Postnummer", formValue: "applicantData.postalCode"},
-        {input: INPUT_TYPE.TEXT_FIELD, label: "Poststed", formValue: "applicantData.city"},
-        {input: INPUT_TYPE.TEXT_FIELD, label: "Telefonnummer", formValue: "applicantData.phoneNumber"},
-        {input: INPUT_TYPE.TEXT_FIELD, label: "Epost", formValue: "applicantData.email"},
+        {input: INPUT_TYPE.TEXT_FIELD, label: "Navn", formValue: "applicantData.name.value"},
+        {input: INPUT_TYPE.TEXT_FIELD, label: "Adresse", formValue: "applicantData.address.value"},
+        {input: INPUT_TYPE.TEXT_FIELD, label: "Postnummer", formValue: "applicantData.postalCode.value"},
+        {input: INPUT_TYPE.TEXT_FIELD, label: "Poststed", formValue: "applicantData.city.value"},
+        {input: INPUT_TYPE.TEXT_FIELD, label: "Telefonnummer", formValue: "applicantData.phoneNumber.value"},
+        {input: INPUT_TYPE.TEXT_FIELD, label: "Epost", formValue: "applicantData.email.value"},
         {input: INPUT_TYPE.DROPDOWN, label: "Tilgangskode", value: props.watch("applicantData.accessCode"), formValue: "applicantData.accessCode", dropDownItems: dropdownPlaceholder},
         {input: INPUT_TYPE.DROPDOWN, label: "Hjemmel", value: props.watch("applicantData.paragraph"), formValue: "applicantData.paragraph", dropDownItems: dropdownPlaceholder}
     ]
