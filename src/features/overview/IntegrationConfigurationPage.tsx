2845b9cd8a86dbd0c7b6865a259136fb8540a40d
ingrie <ingride89@gmail.com>
2021-12-29 19:36:44 +0100

    FST-108 integration page details

--- src/features/overview/IntegrationConfigurationPage.tsx
+++ src/features/overview/IntegrationConfigurationPage.tsx
@@ -1,12 +1,13 @@
 import {
     Box,
+    Card,
     Typography,
     Button,
     MenuItem,
     FormControl,
     Select,
     InputLabel,
-    SelectChangeEvent
+    SelectChangeEvent, CardContent
 } from "@mui/material";
 import * as React from "react";
 import {useEffect, useState} from "react";
@@ -43,25 +44,63 @@ const IntegrationConfigurationPage: React.FunctionComponent<any> = (props) => {
 
     return (
         <Box width={750}>
-            <FormControl size='small' sx={{float: 'right', width: 300, mb: 5}}>
-                <InputLabel id="demo-simple-select-label">Versjon</InputLabel>
-                <Select
-                    labelId="version-select-label"
-                    id="version-select"
-                    value={version}
-                    label="Versjon"
-                    onChange={handleChange}
-                >
-                    {versions.map((item: any, index: number) => (
-                        <MenuItem key={index} value={item.value}>{item.label}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <Typography>id: {activeConfiguration.id}</Typography>
-            <Typography>navn: {activeConfiguration.name}</Typography>
-            <Typography>beskrivelse: {activeConfiguration.description}</Typography>
-            <Typography>versjon: {activeConfiguration.version}</Typography>
-            <Typography>orgnummer: {activeConfiguration.applicantConfiguration.organisationNumber}</Typography>
+            <Typography variant={"h5"}>Konfigurasjonsdetaljer</Typography>
+            <Card sx={{mb: 4}}>
+                <CardContent>
+                    <FormControl size='small' sx={{float: 'right', width: 300, mb: 5}}>
+                        <InputLabel id="version-select-input-label">Versjon</InputLabel>
+                        <Select
+                            labelId="version-select-label"
+                            id="version-select"
+                            value={version}
+                            label="Versjon"
+                            onChange={handleChange}
+                        >
+                            {versions.map((item: any, index: number) => (
+                                <MenuItem key={index} value={item.value}>{item.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <Typography>Id: {activeConfiguration.id}</Typography>
+                    <Typography>Navn: {activeConfiguration.name}</Typography>
+                    <Typography>Beskrivelse: {activeConfiguration.description}</Typography>
+                    <Typography>Versjon: {activeConfiguration.version}</Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{mb: 4}}>
+                <CardContent>
+                    <Typography variant={"h6"}>Sakspost</Typography>
+                    <Typography>saksnummer: {activeConfiguration.caseConfiguration.caseNumber}</Typography>
+                    {activeConfiguration.caseConfiguration.fields.map((field: any, index: number) => {
+                        return <Typography key={index}>{field.field}: {field.valueBuilder.value} {field.valueBuilder.properties[0]?.key}</Typography>
+                    })}
+                </CardContent>
+            </Card>
+            <Card sx={{mb: 4}}>
+                <CardContent>
+                    <Typography variant={"h6"}>Journalpost</Typography>
+                    {activeConfiguration.recordConfiguration.fields.map((field: any, index: number) => {
+                        return <Typography key={index}>{field.field}: {field.valueBuilder.value} {field.valueBuilder.properties[0]?.key}</Typography>
+                    })}
+                </CardContent>
+            </Card>
+            <Card sx={{mb: 4}}>
+                <CardContent>
+                    <Typography variant={"h6"}>Dokument- og objektbeskrivelse</Typography>
+                    {activeConfiguration.documentConfiguration.fields.map((field: any, index: number) => {
+                        return <Typography key={index}>{field.field}: {field.valueBuilder.value} {field.valueBuilder.properties[0]?.key}</Typography>
+                    })}
+                </CardContent>
+            </Card>
+            <Card sx={{mb: 4}}>
+                <CardContent>
+                    <Typography variant={"h6"}>Avsender</Typography>
+                    <Typography>orgnummer: {activeConfiguration.applicantConfiguration.organisationNumber}</Typography>
+                    {activeConfiguration.applicantConfiguration.fields.map((field: any, index: number) => {
+                        return <Typography key={index}>{field.field}: {field.valueBuilder.value} {field.valueBuilder.properties[0]?.key}</Typography>
+                    })}
+                </CardContent>
+            </Card>
 
             <Button variant="contained" onClick={props.reset}>Tilbake</Button>
 
