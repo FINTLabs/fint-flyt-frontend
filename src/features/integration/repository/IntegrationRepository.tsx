ba3f3df1d36f867ad20b5a9fae47de6faba54355
Frode Sjovatsen <frode@fintlabs.no>
2022-03-16 21:15:21 +0100

    Let's do it another way ;)

--- src/features/integration/repository/IntegrationRepository.tsx
+++ src/features/integration/repository/IntegrationRepository.tsx
@@ -3,35 +3,35 @@ import IFormData from "../types/Form/FormData";
 import {IIntegrationConfiguration} from "../types/IntegrationConfiguration";
 
 const getLatest = () => {
-    return axios.get<Array<IFormData>>("./api/integration/configuration");
+    return axios.get<Array<IFormData>>("/api/integration/configuration");
 };
 
 const getLatestByID = (id: any) => {
-    return axios.get<Array<IFormData>>(`./api/integration/configuration/${id}/latest`);
+    return axios.get<Array<IFormData>>(`/api/integration/configuration/${id}/latest`);
 }
 
 const get = () => {
-    return axios.get<any>(`./api/integration/configuration/`);
+    return axios.get<any>(`/api/integration/configuration/`);
 };
 
 const getByIdAndVersion = (id: any, version: any) => {
-    return axios.get<any>(`./api/integration/configuration/${id}/${version}`);
+    return axios.get<any>(`/api/integration/configuration/${id}/${version}`);
 };
 
 const create = (data: IIntegrationConfiguration) => {
-    return axios.post<any>("./api/integration/configuration", data);
+    return axios.post<any>("/api/integration/configuration", data);
 };
 
 const update = (id: any, data: IIntegrationConfiguration) => {
-    return axios.put<any>(`./api/integration/configuration/${id}`, data);
+    return axios.put<any>(`/api/integration/configuration/${id}`, data);
 };
 
 const remove = (id: any) => {
-    return axios.delete<any>(`./api/integration/configuration/${id}`);
+    return axios.delete<any>(`/api/integration/configuration/${id}`);
 };
 
 const getSak = (caseYear: any, caseNumber: any) => {
-    return axios.get<any>(`./api/sak/mappeid/${caseYear}/${caseNumber}/tittel`, {timeout: 5000})
+    return axios.get<any>(`/api/sak/mappeid/${caseYear}/${caseNumber}/tittel`, {timeout: 5000})
 }
 
 const IntegrationRepository = {
