17c37f18e545b202894c53a49391e7f0b28caa0b
ingrie <ingride89@gmail.com>
2022-03-25 12:22:58 +0100

    add search form checks

--- cypress/integration/integration_form_test.spec.js
+++ cypress/integration/integration_form_test.spec.js
@@ -7,9 +7,20 @@ describe('Testing Integration Form', () => {
             cy.get('.MuiTypography-h5').should('contain', 'Integrasjon til arkiv')
         });
 
-    it('should have accordion header', () => {
-        cy.get('.MuiPaper-root.Mui-expanded > .MuiAccordionSummary-root > .MuiAccordionSummary-content > .MuiTypography-root').should('contain', 'Integrasjonslogikk')
-        cy.get(':nth-child(2) > .MuiAccordionSummary-root > .MuiAccordionSummary-content > .MuiTypography-root').should('contain', 'Sak')
-    });
+        it('should have accordion headers', () => {
+            cy.get('.MuiPaper-root.Mui-expanded > .MuiAccordionSummary-root > .MuiAccordionSummary-content > .MuiTypography-root').should('contain', 'Integrasjonslogikk')
+            cy.get(':nth-child(2) > .MuiAccordionSummary-root > .MuiAccordionSummary-content > .MuiTypography-root').should('contain', 'Sak')
+            cy.get(':nth-child(3) > .MuiAccordionSummary-root > .MuiAccordionSummary-content > .MuiTypography-root').should('contain', 'Journalpost')
+            cy.get(':nth-child(4) > .MuiAccordionSummary-root > .MuiAccordionSummary-content > .MuiTypography-root').should('contain', 'Dokument- og objektbeskrivelse')
+            cy.get(':nth-child(5) > .MuiAccordionSummary-root > .MuiAccordionSummary-content > .MuiTypography-root').should('contain', 'Avsender')
+        });
+
+        it('should have show case number field on collection strategy', () => {
+            cy.get(':nth-child(3) > .MuiFormControlLabel-root > .MuiRadio-root > .PrivateSwitchBase-input').click()
+            cy.get('#mui-41').should('be.visible')
+            cy.get('#mui-41').type('2021/06')
+            cy.get('.css-0 > .MuiButton-root').click()
+            cy.get('.css-16u1nra-MuiTypography-root').should('be.visible')
+        });
     }
 )
