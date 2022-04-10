87706265d2e3b968db00c1e45d5d7f902ee01f64
ingrie <ingride89@gmail.com>
2022-04-10 19:07:41 +0200

    sourceAppForm component and add noNN

--- src/features/integration/components/SourceApplicationForm.tsx
+++ src/features/integration/components/SourceApplicationForm.tsx
@@ -3,12 +3,14 @@ import {Tag} from "./dnd/Tag";
 import {sourceApplicationFormPopoverContent, tagList} from "../defaults/DefaultValues";
 import * as React from "react";
 import HelpPopover from "./popover/HelpPopover";
+import { useTranslation } from "react-i18next";
 
 const SourceApplicationForm: React.FunctionComponent<any> = (props) => {
+    const { t } = useTranslation('translations', { keyPrefix: 'components.SourceApplicationForm'});
     return (
         <Box className={props.style.sourceApplicationForm}>
             <Box className={props.style.row}>
-                <Typography variant={"h6"}>Data fra skjema</Typography>
+                <Typography variant={"h6"}>{t('header')}</Typography>
                 <HelpPopover popoverContent="sourceApplicationFormPopoverContent"/>
             </Box>
             {tagList.map((tag, index) => {
