ab3a27392d996957fc258c284ad19e47ab5f0ff7
Ingrid Evensen <ingride89@gmail.com>
2022-07-13 10:28:51 +0200

    Event log wip (#45)
    * Demo
    
    * change density and pull master
    
    * add test events
    
    * add WIP header

---
+++ src/__tests__/mock/mock-hendelser-data.ts
@@ -0,0 +1,161 @@
+import {IEvent} from "../../features/log/types/Event";
+
+export const MOCK_HENDELSER: IEvent[] = [
+    {
+        "instanceFlowHeaders": {
+            "orgId":"df5f5006-736e-47b0-9d58-2e3330fa7a87",
+            "service":"service",
+            "sourceApplication":"ACOS",
+            "sourceApplicationIntegrationId":"VIK026",
+            "sourceApplicationInstanceId":"42t63",
+            "integrationId": "integration-id",
+            "correlationId":"1",
+            "instanceId":"null",
+            "configurationId":"null",
+            "caseId":"null",
+            "dispatchId":"null"
+        },
+        "name":"incoming-instance",
+        "timeStamp": "2022-01-06T18:08:00.00",
+        "type":"INFO",
+        "errors":[]
+    },
+    {
+        "instanceFlowHeaders":{
+            "orgId":"3dc51477-12ca-4cb7-a717-2b28b462a1cc",
+            "service":"service",
+            "sourceApplication":"ACOS",
+            "sourceApplicationIntegrationId":"VIK036",
+            "sourceApplicationInstanceId":"93875",
+            "integrationId": "integration-id",
+            "correlationId":"1",
+            "instanceId":"1",
+            "configurationId":"null",
+            "caseId":"null",
+            "dispatchId":"null"
+        },
+        "name":"new-instance",
+        "timeStamp": "2022-01-03T06:08:01.956935",
+        "type":"INFO",
+        "errors":[]
+    },
+    {
+        "instanceFlowHeaders":{
+            "orgId":"54bd03a6-0bf2-42da-97d8-ed7d749d8be0",
+            "service":"service",
+            "sourceApplication":"ACOS",
+            "sourceApplicationIntegrationId":"VIK201",
+            "sourceApplicationInstanceId":"34982",
+            "integrationId": "integration-id",
+            "correlationId":"1",
+            "instanceId":"1",
+            "configurationId":"1",
+            "caseId":"null",
+            "dispatchId":"null"
+        },
+        "name":"instance-to-case-mapping-error",
+        "timeStamp": "2022-03-06T11:08:01.95695",
+        "type":"ERROR",
+        "errors":[{
+            "errorCode":"0",
+            "args":{
+                "arg0":"manglende felt i skjema",
+                "arg1":"'fornavn'"
+            }
+        }, {
+            "errorCode":"1",
+            "args":{
+                "arg0":"feil i mapping mellom felt i instans og skjema. felt:",
+                "arg1":"'administrativ enhet'"
+            }
+        }]
+    },
+    {
+        "instanceFlowHeaders":{
+            "orgId":"3dc51477-12ca-4cb7-a717-2b28b462a1cc",
+            "service":"service",
+            "sourceApplication":"ACOS",
+            "sourceApplicationIntegrationId":"VIK031",
+            "sourceApplicationInstanceId":"1225",
+            "integrationId": "integration-id",
+            "correlationId":"1",
+            "instanceId":"1",
+            "configurationId":"null",
+            "caseId":"null",
+            "dispatchId":"null"
+        },
+        "name":"new-instance",
+        "timeStamp": "2022-05-04T12:08:01.956935",
+        "type":"INFO",
+        "errors":[]
+    },
+    {
+        "instanceFlowHeaders": {
+            "orgId":"feearrg-736e-47b0-9d58-gsera4532",
+            "service":"service",
+            "sourceApplication":"ACOS",
+            "sourceApplicationIntegrationId":"VIK076",
+            "sourceApplicationInstanceId":"42t63",
+            "integrationId": "integration-id",
+            "correlationId":"1",
+            "instanceId":"null",
+            "configurationId":"null",
+            "caseId":"null",
+            "dispatchId":"null"
+        },
+        "name":"incoming-instance",
+        "timeStamp": "2022-01-06T19:08:00.00",
+        "type":"INFO",
+        "errors":[]
+    },
+    {
+        "instanceFlowHeaders":{
+            "orgId":"754gsdfrg-0bf2-42da-97d8-2347edfgd",
+            "service":"service",
+            "sourceApplication":"ACOS",
+            "sourceApplicationIntegrationId":"VIK014",
+            "sourceApplicationInstanceId":"34982",
+            "integrationId": "integration-id",
+            "correlationId":"1",
+            "instanceId":"1",
+            "configurationId":"1",
+            "caseId":"null",
+            "dispatchId":"null"
+        },
+        "name":"instance-to-case-mapping-error",
+        "timeStamp": "2022-03-06T11:08:01.95695",
+        "type":"ERROR",
+        "errors":[{
+            "errorCode":"0",
+            "args":{
+                "arg0":"manglende obligatorisk felt i skjema",
+                "arg1":"'fødselsnummer'"
+            }
+        }]
+    },
+    {
+        "instanceFlowHeaders":{
+            "orgId":"feay55-df4y-42da-97d8-345qrgga4",
+            "service":"service",
+            "sourceApplication":"ACOS",
+            "sourceApplicationIntegrationId":"VIK163",
+            "sourceApplicationInstanceId":"2312",
+            "integrationId": "integration-id",
+            "correlationId":"1",
+            "instanceId":"1",
+            "configurationId":"1",
+            "caseId":"null",
+            "dispatchId":"null"
+        },
+        "name":"connection-error",
+        "timeStamp": "2022-03-06T11:08:01.95695",
+        "type":"ERROR",
+        "errors":[{
+            "errorCode":"0",
+            "args":{
+                "arg0":"mangler data fra",
+                "arg1":"'Elements'"
+            }
+        }]
+    },
+]
