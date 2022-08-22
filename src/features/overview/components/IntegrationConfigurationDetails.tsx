924f98b62999364e0b25c055a1b80956fa003ec3
ingrie <ingride89@gmail.com>
2022-08-22 12:05:23 +0200

    access code and paragraph fields dependant on protected or not

--- src/features/overview/components/IntegrationConfigurationDetails.tsx
+++ src/features/overview/components/IntegrationConfigurationDetails.tsx
@@ -167,9 +167,9 @@ const IntegrationConfigurationDetails: React.FunctionComponent<any> = (props) =>
                         <Typography><strong>{t('labels.contactPerson')}</strong>{activeFormData.applicantData?.contactPerson}</Typography>
                         <Typography><strong>{t('labels.phoneNumber')}</strong>{activeFormData.applicantData?.phoneNumber}</Typography>
                         <Typography><strong>{t('labels.email')}</strong>{activeFormData.applicantData?.email}</Typography>
+                        <Typography><strong>{t('labels.protected')}</strong>{activeFormData.applicantData?.protected ? 'Ja' : 'Nei'}</Typography>
                         <Typography><strong>{t('labels.accessCode')}</strong>{activeFormData.applicantData?.accessCode}</Typography>
                         <Typography><strong>{t('labels.paragraph')}</strong>{activeFormData.applicantData?.paragraph}</Typography>
-                        <Typography><strong>{t('labels.protected')}</strong>{activeFormData.applicantData?.protected ? 'Ja' : 'Nei'}</Typography>
                     </CardContent>
                 </Card>
                 <Button variant="contained" onClick={props.reset}>{t('button.back')}</Button>
