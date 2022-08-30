c02d882fba9734df3576051058704ba673535502
ingrie <ingride89@gmail.com>
2022-08-30 09:45:43 +0200

    FST-247

--- src/features/instanceOverview/InstanceOverview.tsx
+++ src/features/instanceOverview/InstanceOverview.tsx
@@ -1,31 +1,14 @@
-import {Theme, Typography} from '@mui/material';
+import {Button, Typography} from '@mui/material';
 import React, {useEffect, useState} from 'react';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
-import {createStyles, makeStyles} from "@mui/styles";
 import { useTranslation } from 'react-i18next';
 import Box from "@mui/material/Box";
-import {DataGrid, GridColumns, GridToolbar} from "@mui/x-data-grid";
+import {DataGrid, GridCellParams, GridColumns, GridToolbar} from "@mui/x-data-grid";
 import {gridLocaleNoNB} from "../util/locale/gridLocaleNoNB";
 import {IEvent} from "../log/types/Event";
 import moment from "moment";
 import EventRepository from "../log/repository/EventRepository";
 import {addId} from "../util/JsonUtil";
-
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        form: {
-            width: theme.spacing(120)
-        },
-        row: {
-            display: 'flex'
-        },
-        dataGridBox: {
-            height: "750px",
-            width: '100%'
-        }
-    })
-);
 
 const InstanceOverview: React.FunctionComponent<RouteComponentProps<any>> = () => {
     const {t} = useTranslation('translations', {keyPrefix: 'pages.instanceOverview'})
@@ -41,12 +24,25 @@ const InstanceOverview: React.FunctionComponent<RouteComponentProps<any>> = () =
         },
         { field: 'sourceApplicationIntegrationId', type: 'string', headerName: 'Skjema', flex: 3,
             valueGetter: (params) => params.row.instanceFlowHeaders.sourceApplicationIntegrationId
-        }
+        },
+        { field: 'details', headerName: 'Resend', flex: 1, sortable: false, filterable: false,
+            renderCell: (params) => ( <CustomButtonToggle row={params.row} />)}
     ];
 
     useEffect(()=> {
         getAllEvents();
     }, []);
+
+    function CustomButtonToggle(props: GridCellParams["row"]) {
+        const hasErrors: boolean = props.row.errors.length > 0;
+        return (
+            <>
+                {hasErrors &&
+                    <Button size="small" variant="outlined">Send</Button>
+                }
+            </>
+        );
+    }
 
     const getAllEvents = () => {
         EventRepository.getEvents()
