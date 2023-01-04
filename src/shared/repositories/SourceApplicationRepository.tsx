8df2f024caf113d067112a965dbf455b051157e3
ingridevensen <ingride89@gmail.com>
2023-01-04 09:54:55 +0100

    pull localUrls and add /

--- src/shared/repositories/SourceApplicationRepository.tsx
+++ src/shared/repositories/SourceApplicationRepository.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const getMetadata = (id: string, onlyLastestVersion?: boolean) => {
-    return axios.get<any>("api/intern/metadata", {params: {kildeapplikasjonId: id, bareSisteVersjoner: onlyLastestVersion}})
+    return axios.get<any>("/api/intern/metadata", {params: {kildeapplikasjonId: id, bareSisteVersjoner: onlyLastestVersion}})
 };
 
 const getInstanceElementMetadata = (metadataId: string) => {
-    return axios.get<any>(`api/intern/metadata/${metadataId}/instans-element-metadata`)
+    return axios.get<any>(`/api/intern/metadata/${metadataId}/instans-element-metadata`)
 }
 
 const SourceApplicationRepository = {
