fdd7dba8e363b61fd4569c4e7201883d79302362
Egil Ballestad <egil.ballestad@vigoiks.no>
2023-01-05 10:43:25 +0100

    readd setupProxy and slash on api urls

--- src/shared/repositories/ResourceRepository.tsx
+++ src/shared/repositories/ResourceRepository.tsx
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const getClasses = (link: string) => {
-    return axios.get<any>(`api/intern/arkiv/kodeverk/klasse/`, {params: {klassifikasjonssystemLink: link}});
+    return axios.get<any>(`/api/intern/arkiv/kodeverk/klasse/`, {params: {klassifikasjonssystemLink: link}});
 };
 
 const getResource = (resource: string) => {
-    return axios.get<any>(`api/intern/arkiv/kodeverk/${resource}`);
+    return axios.get<any>(`/api/intern/arkiv/kodeverk/${resource}`);
 }
 
 const getSak = (caseYear: any, caseNumber: any) => {
-    return axios.get<any>(`api/intern/arkiv/saker/${caseYear}/${caseNumber}/tittel`, {timeout: 10000})
+    return axios.get<any>(`/api/intern/arkiv/saker/${caseYear}/${caseNumber}/tittel`, {timeout: 10000})
 }
 const ResourceRepository = {
     getClasses,
