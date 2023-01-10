9362a895bb3b6ca9b87cccc12a314a181d437dfb
Egil Ballestad <egil.ballestad@vigoiks.no>
2023-01-10 11:00:12 +0100

    add continuous deployment for viken-no to alpha and beta clusters

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -8,11 +8,12 @@ import {
     Typography
 } from "@mui/material";
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import FintLogo from "../../images/fint-by-vigo-white.svg";
 
 import {createStyles, makeStyles} from "@mui/styles";
 import Router from "./Router";
 import MenuItems from "./MenuItems";
-import {Link as RouterLink} from "react-router-dom";
+import {BrowserRouter, Link as RouterLink} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {IntegrationContext} from "../../context/integrationContext";
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
@@ -73,6 +74,7 @@ const useStyles = makeStyles((theme: Theme) =>
         }
     }));
 
+const getBasename = (path: string) => path.substring(0, path.lastIndexOf('/'));
 
 
 function Main() {
@@ -86,43 +88,48 @@ function Main() {
     const { isAdmin, setIsAdmin } = useContext(SourceApplicationContext)
     const {statistics} = useContext(IntegrationContext)
     let totalErrors = 0;
-    statistics?.map((stat: any) => {totalErrors += stat.currentErrors})
+    statistics?.map((stat: any) => {
+        totalErrors += stat.currentErrors
+    })
 
 
     return (
-        <Box display="flex" position="relative" width={1} height={1}>
-            <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
-                <Toolbar className={classes.toolbar} id={"toolbar"}>
-                    <img src="https://cdn.flais.io/media/fint-by-vigo-white.svg" alt="logo" className={classes.logo}/>
-                    <Typography variant="h6" color="inherit" noWrap className={classes.flex}>
-                        {t('appbarHeader')}
-                    </Typography>
-                    {/*<Box sx={{ mr: 2 }}>
+        <BrowserRouter basename={getBasename(window.location.pathname)}>
+
+            <Box display="flex" position="relative" width={1} height={1}>
+                <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
+                    <Toolbar className={classes.toolbar} id={"toolbar"}>
+                        <img src={FintLogo} alt="logo" className={classes.logo}/>
+                        <Typography variant="h6" color="inherit" noWrap className={classes.flex}>
+                            {t('appbarHeader')}
+                        </Typography>
+                        {/*<Box sx={{ mr: 2 }}>
                         {i18n.language === 'no' && <Button size="small" variant="contained" onClick={() => changeLanguage("en")}>{t('language.english')}</Button>}
                         {i18n.language === 'en' && <Button size="small" variant="contained" onClick={() => changeLanguage("no")}>{t('language.norwegian')}</Button>}
                     </Box>*/}
-                    {isAdmin && <Box sx={{ mr: 2}}>
-                        <Button size="medium" variant="contained" component={RouterLink} to="/admin" endIcon={<AdminPanelSettingsIcon />}
-                        >
-                            {t('adminHeader')}
-                        </Button>
-                    </Box>}
-                    <Badge className={classes.badge}
-                           badgeContent={totalErrors}
-                           color="secondary"
-                           component={RouterLink} to="/integration/instance/list">
-                        <NotificationsIcon htmlColor={"white"}/>
-                    </Badge>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" className={classes.drawer}>
-                <Toolbar/>
-                <MenuItems/>
-            </Drawer>
-            <main className={classes.content}>
-                <Router/>
-            </main>
-        </Box>
+                        {isAdmin && <Box sx={{ mr: 2}}>
+                            <Button size="medium" variant="contained" component={RouterLink} to="/admin" endIcon={<AdminPanelSettingsIcon />}
+                            >
+                                {t('adminHeader')}
+                            </Button>
+                        </Box>}
+                        <Badge className={classes.badge}
+                               badgeContent={totalErrors}
+                               color="secondary"
+                               component={RouterLink} to="/integration/instance/list">
+                            <NotificationsIcon htmlColor={"white"}/>
+                        </Badge>
+                    </Toolbar>
+                </AppBar>
+                <Drawer variant="permanent" className={classes.drawer}>
+                    <Toolbar/>
+                    <MenuItems/>
+                </Drawer>
+                <main className={classes.content}>
+                    <Router/>
+                </main>
+            </Box>
+        </BrowserRouter>
     );
 }
 
