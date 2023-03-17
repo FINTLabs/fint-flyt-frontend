b7348d2a50b6676f5fd615f2d55748361d1cd0be
ingridevensen <ingride89@gmail.com>
2023-03-17 14:21:49 +0100

    pull 606

--- src/features/configuration/components/common/SearchSelectValueComponent.tsx
+++ src/features/configuration/components/common/SearchSelectValueComponent.tsx
@@ -40,7 +40,6 @@ const SearchSelectValueComponent: React.FunctionComponent<Props> = (props: Props
                     )}
                     onChange={(_, data) => {
                         data !== null ? field.onChange(data.value) : field.onChange(data)
-                        console.log(data)
                     }}
                 />
             )}
