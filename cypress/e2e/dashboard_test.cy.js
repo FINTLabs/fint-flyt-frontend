fbad0cdc1187582815e10bec3ffbce703b70b05f
ingridevensen <ingride89@gmail.com>
2023-06-12 13:45:36 +0200

    add link tests to dashboard

--- cypress/e2e/dashboard_test.cy.js
+++ cypress/e2e/dashboard_test.cy.js
@@ -3,6 +3,8 @@ describe('Testing dashboard', () => {
         cy.intercept('GET', '**/integrasjoner?side=0&sorteringFelt=state&sorteringRetning=ASC', { fixture: 'integrations.json' }).as('getIntegrations')
         cy.intercept('GET', '**/historikk/statistikk/integrasjoner', { fixture: 'historikk.json' }).as('getHistory')
         cy.intercept('GET', '**/metadata?kildeapplikasjonId=2&bareSisteVersjoner=true', { fixture: 'metadata.json' }).as('getMetadata')
+        cy.intercept('GET', '**/integrasjoner', { fixture: 'integrations.json' }).as('getIntegrations')
+        cy.intercept('GET', '**/historikk/hendelser?side=0&antall=10000&sorteringFelt=timestamp&sorteringRetning=DESC&bareSistePerInstans=true', { fixture: 'hendelser.json' }).as('getHendelser')
     })
 
     function prep() {
@@ -14,5 +16,14 @@ describe('Testing dashboard', () => {
     it('should show datagrid', () => {
         prep()
         cy.get('.MuiDataGrid-root').should("be.visible")
+    })
+
+    it('should open new integration component on link click', () => {
+        prep()
+        cy.get('#dashboard-card-0-btn-0').click()
+    })
+    it('should open instance list component on link click', () => {
+        prep()
+        cy.get('#dashboard-card-1-btn-0').click()
     })
 });
