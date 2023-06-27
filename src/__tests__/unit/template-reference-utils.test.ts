69c41a8d0d5f80723851fe99116c8f0d775c7a50
Eivind Morch <eivind@morch.priv.no>
2023-06-27 15:14:21 +0200

    FF-677 FF-755 #comment Add tests and improve test names

--- src/__tests__/unit/template-reference-utils.test.ts
+++ src/__tests__/unit/template-reference-utils.test.ts
@@ -1,41 +1,73 @@
 import {getTemplatePathFromTemplateReference} from "../../features/configuration/util/TemplateReferenceUtils";
 
 
-describe('', () => {
+describe('Template reference utils', () => {
 
-    test('template path from template reference 1', () => {
-        const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
-        const reference: string = '/sak.journalposter.1.tittel';
-        expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
-            .toEqual(['sak', 'journalposter', '1', 'tittel'])
-    });
+    describe('getTemplatePathFromTemplateReference', () => {
 
-    test('template path from template reference 2', () => {
-        const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
-        const reference: string = 'tittel';
-        expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
-            .toEqual(['sak', 'journalposter', '1', 'tittel'])
-    });
+        test('static reference', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
+            const reference: string = '/sak.journalposter.1.tittel';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '1', 'tittel'])
+        });
 
-    test('template path from template reference 3', () => {
-        const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
-        const reference: string = '../n.tittel';
-        expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
-            .toEqual(['sak', 'journalposter', '1', 'tittel'])
-    });
+        test('reference with single component', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
+            const reference: string = 'tittel';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '1', 'tittel'])
+        });
 
-    test('template path from template reference 4', () => {
-        const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
-        const reference: string = '../n+1.tittel';
-        expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
-            .toEqual(['sak', 'journalposter', '2', 'tittel'])
-    });
+        test('reference with multiple components', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
+            const reference: string = 'part.tittel';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '1', 'part', 'tittel'])
+        });
 
-    test('template path from template reference 5', () => {
-        const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
-        const reference: string = '../n-1.tittel';
-        expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
-            .toEqual(['sak', 'journalposter', '0', 'tittel'])
-    });
+        test('reference with only dynamic path level', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1', 'part'];
+            const reference: string = '../';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '1'])
+        });
+
+        test('reference with single dynamic path level', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1', 'part'];
+            const reference: string = '../tittel';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '1', 'tittel'])
+        });
+
+        test('reference with multiple dynamic path levels', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1', 'part'];
+            const reference: string = '../../2';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '2'])
+        });
+
+        test('reference with dynamic collection index n', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
+            const reference: string = '../n.tittel';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '1', 'tittel'])
+        });
+
+        test('reference with dynamic collection index n+1', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
+            const reference: string = '../n+1.tittel';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '2', 'tittel'])
+        });
+
+        test('reference with dynamic collection index n-1', () => {
+            const referenceOriginPath: string[] = ['sak', 'journalposter', '1'];
+            const reference: string = '../n-1.tittel';
+            expect(getTemplatePathFromTemplateReference(referenceOriginPath, reference))
+                .toEqual(['sak', 'journalposter', '0', 'tittel'])
+        });
+    })
+
 
 })
