f0c436788eb03b917b135840d1a801f047832847
ingridevensen <ingride89@gmail.com>
2023-08-22 17:27:48 +0200

    FFS-303 fix tests

--- src/__tests__/unit/string-util.test.ts
+++ src/__tests__/unit/string-util.test.ts
@@ -4,7 +4,8 @@ import {IError, IErrorArg} from "../../features/instanceOverview/types/Event";
 const testErrorArgs: Record<string, string> = {
     'instanceFieldKey': 'form.title',
     'fieldPath': 'Here is the error, and ',
-    'errorMessage': 'On a scale of 1-10 my friend, you\'re f\'ed'
+    'errorMessage': 'On a scale of 1-10 my friend, you\'re f\'ed',
+    'message': 'Ingen støtte for dette domenet'
 }
 
 const testError: IError = {
@@ -16,13 +17,15 @@ const testError: IError = {
 const resultingErrorArgs: IErrorArg[] = [
     {value: 'form.title', type: 'instanceFieldKey'},
     {value: 'Here is the error, and ', type: 'fieldPath'},
-    {value: 'On a scale of 1-10 my friend, you\'re f\'ed', type: 'errorMessage'}
+    {value: 'On a scale of 1-10 my friend, you\'re f\'ed', type: 'errorMessage'},
+    {value: 'Ingen støtte for dette domenet', type: 'message'}
 ]
 
 const testStrings: string[] = [
     "Valideringsfeil i mottak av instans, '#fieldPath# #errorMessage#'",
     "Mangler felt ‘#instanceFieldKey#’ i instans, som kreves for å sette felt i sak",
     "Instansen ble avvist av destinasjon med følgende feilmelding: '#errorMessage#'",
+    "Instans avvist, '#message#'"
 ]
 const errorArgs: IErrorArg[] = [
     {value: 'foo', type: 'fieldPath'},
@@ -38,4 +41,5 @@ test('It should show error messages correctly', () => {
     expect(errorStringReplace(testStrings[0], errorArgs)).toEqual("Valideringsfeil i mottak av instans, 'foo bar'")
     expect(errorStringReplace(testStrings[1], errorArgs)).toEqual("Mangler felt ‘qux’ i instans, som kreves for å sette felt i sak")
     expect(errorStringReplace(testStrings[2], resultingErrorArgs)).toEqual("Instansen ble avvist av destinasjon med følgende feilmelding: 'On a scale of 1-10 my friend, you're f'ed'")
+    expect(errorStringReplace(testStrings[3], resultingErrorArgs)).toEqual("Instans avvist, 'Ingen støtte for dette domenet'")
 });
