591c8c1214af4b4095db0f2988a6c9efa8b1bab5
ingridevensen <ingride89@gmail.com>
2023-09-12 17:12:17 +0200

    FFS-345 dynamiske felter validering

--- src/features/configuration/util/ValidationUtil.ts
+++ src/features/configuration/util/ValidationUtil.ts
@@ -1,58 +1,36 @@
-import {ValidationRule} from "react-hook-form";
 import {ValueType} from "../types/Configuration";
 
 export const numberPattern = /\d+/;
 export const valueConverterReferencePattern = new RegExp(`\\$vc\\{${numberPattern.source}\\}`);
-
 export const instanceValueKeyPattern = new RegExp("(?:(?!\\$if\\{).)+");
 export const ifReferencePattern = new RegExp(`(?:\\$if\\{${instanceValueKeyPattern.source}\\})*`);
-
-
 export const textPattern = new RegExp("(?:(?!\\$if\\{).)*");
-
-export const dynamicStringPattern = new RegExp(`^(?:${textPattern.source}|${ifReferencePattern.source})*$`);
 export const instanceCollectionFieldReferencePattern = new RegExp(`\\$icf\\{${numberPattern.source}\\}\\{${instanceValueKeyPattern.source}\\}`);
+export const dynamicStringPattern = new RegExp(`^(?:${textPattern.source}|${ifReferencePattern.source}|${ifReferencePattern.source})*$`);
 
-
-export const valueConvertingPattern = new RegExp(`^${valueConverterReferencePattern.source}(${ifReferencePattern.source}|${instanceCollectionFieldReferencePattern.source})$`);
-
+export const icfPattern = /^\$vc\{\d+\}((?:\$if\{(?:(?!\$if\{).)+\})*|\$icf\{\d+\}\{(?:(?!\$if\{).)+\})$/
 export const combinedCollectionPattern = /^(?:(\$if\{[^}]+\})|(\$icf\{\d+}{[^}]+\}))$/;
 
-
-export function getRegexFromType(type: ValueType, completed: boolean, collection?: boolean): ValidationRule<RegExp> | undefined {
-    if (!completed) {
-        return undefined
-    }
-    if (collection) {
-        if (type === ValueType.DYNAMIC_STRING) {
-            return {
-                value: combinedCollectionPattern,
-                message: 'Oppfyller ikke påkrevd format'
-            }
-        } else return undefined
+export const hasValidFormat = (value: string, type: ValueType, completeCheck: boolean, collection?: boolean) => { //eslint-disable-line
+    if (!completeCheck) {
+        return true
     }
 
-    switch (type) {
-        case ValueType.DYNAMIC_STRING:
-            return {
-                value: dynamicStringPattern,
-                message: 'Oppfyller ikke påkrevd format'
-            }
-        case ValueType.VALUE_CONVERTING:
-            return {
-                value: valueConvertingPattern,
-                message: 'Oppfyller ikke påkrevd format for verdikonvertering'
-            }
-        case ValueType.FILE:
-            type
-            return {
-                value: dynamicStringPattern,
-                message: 'Oppfyller ikke påkrevd format'
-            }
-        case ValueType.BOOLEAN:
-        case ValueType.STRING:
-        case ValueType.URL:
-        default:
-            return undefined
+    if (collection && type === ValueType.DYNAMIC_STRING) {
+        return combinedCollectionPattern.test(value)
     }
-}
+
+    if (type === ValueType.VALUE_CONVERTING) {
+        return icfPattern.test(value)
+    }
+
+    if (type === ValueType.DYNAMIC_STRING) {
+        const ifRefCount = (value.match(/\$if\{/g) || []).length
+        if(ifRefCount && ifRefCount > 0) {
+            const ifRefCloserCount = (value.match(/}/g) || []).length
+            return ifRefCount === ifRefCloserCount
+        }
+        return true
+    }
+    return true;
+};
