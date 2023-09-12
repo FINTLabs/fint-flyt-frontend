c6e83c0f6be111333f87330ca98b72ecb3a5a2ab
ingridevensen <ingride89@gmail.com>
2023-09-12 10:44:31 +0200

    FFS-417 bug on validation

--- src/features/configuration/util/ValidationUtil.ts
+++ src/features/configuration/util/ValidationUtil.ts
@@ -6,19 +6,15 @@ export const valueConverterReferencePattern = new RegExp(`\\$vc\\{${numberPatter
 export const instanceValueKeyPattern = new RegExp("(?:(?!\\$if\\{).)+");
 export const ifReferencePattern = new RegExp(`(?:\\$if\\{${instanceValueKeyPattern.source}\\})*`);
 
-
 export const textPattern = new RegExp("(?:(?!\\$if\\{).)*");
 
 export const instanceCollectionFieldReferencePattern = new RegExp(`\\$icf\\{${numberPattern.source}\\}\\{${instanceValueKeyPattern.source}\\}`);
 export const dynamicStringPattern = new RegExp(`^(?:${textPattern.source}|${ifReferencePattern.source}|${ifReferencePattern.source})*$`);
 
-
-export const valueConvertingPattern = new RegExp(`^${valueConverterReferencePattern.source}(${ifReferencePattern.source}|${instanceCollectionFieldReferencePattern.source})$`);
-
-export const combinedCollectionPattern = /^(?:(\$if\{[^}]+\})|(\$icf\{\d+}{[^}]+\}))$/;
-
-
 export const hasValidFormat = (value: any, type: ValueType, completeCheck: boolean, collection?: boolean) => { //eslint-disable-line
+    const icfPattern = /^\$vc\{\d+\}((?:\$if\{(?:(?!\$if\{).)+\})*|\$icf\{\d+\}\{(?:(?!\$if\{).)+\})$/
+    const combinedCollectionPattern = /^(?:(\$if\{[^}]+\})|(\$icf\{\d+}{[^}]+\}))$/;
+
     if (!completeCheck) {
         return undefined
     }
@@ -26,8 +22,6 @@ export const hasValidFormat = (value: any, type: ValueType, completeCheck: boole
     if(collection && type === ValueType.DYNAMIC_STRING) {
         return combinedCollectionPattern.test(value)
     }
-
-    const icfPattern = /^\$vc\{\d+\}((?:\$if\{(?:(?!\$if\{).)+\})*|\$icf\{\d+\}\{(?:(?!\$if\{).)+\})$/
 
     if (type === ValueType.VALUE_CONVERTING) {
         return icfPattern.test(value)
