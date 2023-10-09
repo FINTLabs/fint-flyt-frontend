72b70fba55817a52456ebaa733919782fcb18b4d
hansodeg <hansodeg@gmail.com>
2023-10-09 12:14:13 +0200

    oppdaterte render fra reactDOM til createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './util/locale/i18n.ts';
+import './util/locale/i18n';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
+const container  = document.getElementById('root');
+
+const root = container ? createRoot(container) : null;
+
+root?.render(
+  container ? (
     <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+      <App />
+    </React.StrictMode>
+  ) : null
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
