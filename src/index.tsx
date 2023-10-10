f116d9dbdbddff729a091907dc245e4713b006c4
hansodeg <hansodeg@gmail.com>
2023-10-10 12:16:15 +0200

    la inn browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './util/locale/i18n.ts';
+import {BrowserRouter} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+</React.StrictMode>
 );
 reportWebVitals();
