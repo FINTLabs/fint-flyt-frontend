5c0b7bcdb0184ec276cc0328ee10bc12a8042f64
ingridevensen <ingride89@gmail.com>
2023-11-14 09:00:05 +0100

    split tables

--- src/util/styles/Integration.styles.ts
+++ src/util/styles/Integration.styles.ts
@@ -6,17 +6,16 @@ export const IntegrationStyles = makeStyles((theme: Theme) =>
     createStyles({
         ...useCommonStyles(theme),
         dataPanelBox: {
-            height: theme.spacing(45),
+            height: theme.spacing(55),
             width: '100%',
-            backgroundColor: 'white',
-            marginBottom: theme.spacing(3)
+            marginBottom: theme.spacing(3),
+            "&:last-child": {
+                marginLeft: theme.spacing(2)
+            }
         },
         tableWrapper: {
             maxWidth: theme.spacing(220),
-            border: '1px solid black',
             borderRadius: theme.spacing(0.5),
-            padding: theme.spacing(2),
-            backgroundColor: 'white'
         },
         integrationWrapper: {
             height: theme.spacing(22),
