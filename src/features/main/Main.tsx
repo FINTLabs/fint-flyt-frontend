9d9614366963de0f278f4bcfe2b1a7dacd151b9a
ingridevensen <ingride89@gmail.com>
2023-11-15 12:52:11 +0100

    remove unused code

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {AppBar, Box, Button, Toolbar} from "@mui/material";
 import MenuItems from "./MenuItems";
 import {Link as RouterLink} from "react-router-dom";
-import {useTranslation} from "react-i18next";
 import {MainStyles} from "../../util/styles/Main.styles";
 import ConfigurationProvider from "../../context/ConfigurationContext";
 import Router from "./Router";
@@ -11,12 +10,6 @@ const useStyles = MainStyles;
 
 function Main() {
     const classes = useStyles();
-    const {t, i18n} = useTranslation();
-
-    // eslint-disable-next-line
-    const changeLanguage = (lng: string) => {
-        i18n.changeLanguage(lng);
-    };
 
     return (
         <Box display="flex" position="relative" width={1} height={1}>
@@ -29,10 +22,6 @@ function Main() {
                     <nav>
                         <MenuItems/>
                     </nav>
-                    {/*<Box sx={{ mr: 2 }}>
-                        {i18n.language === 'no' && <Button size="small" variant="contained" onClick={() => changeLanguage("en")}>{t('language.english')}</Button>}
-                        {i18n.language === 'en' && <Button size="small" variant="contained" onClick={() => changeLanguage("no")}>{t('language.norwegian')}</Button>}
-                    </Box>*/}
                 </Toolbar>
             </AppBar>
             <main className={classes.content}>
