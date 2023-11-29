ffeb488d5ebb506d52ea738555d1e0fe3c7d67b6
hansodeg <hansodeg@gmail.com>
2023-11-29 20:54:54 +0100

    La til versjons-side med tilhørende text

---
+++ src/components/pages/Version.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {RouteComponent} from "../../features/main/Route";
+import {BodyLong,  Box, Heading,  VStack} from "@navikt/ds-react";
+import SupportTemplate from "../templates/SupportTemplate";
+import {Contact} from "../atoms/Contact";
+import {ABOUT_VERSIONS, NOVEMBER23} from "../utils/version/VersionTexts";
+
+const Version: RouteComponent = () => {
+
+    return (
+        <SupportTemplate keyPrefix={'pages.version'}>
+            <Box id={"support-information"} background={"surface-default"} padding="6" borderRadius={"large"} borderWidth="2"
+                 borderColor={"border-subtle"}>
+                <VStack gap={"6"}>
+                    <Heading size={"small"}>
+                        Versjonering av FLYT
+                    </Heading>
+                    <BodyLong>
+                        {ABOUT_VERSIONS}
+                    </BodyLong>
+                </VStack>
+            </Box>
+            <Box id={"support-information"} background={"surface-default"} padding="6" borderRadius={"large"} borderWidth="2"
+                 borderColor={"border-subtle"}>
+                <VStack gap={"6"}>
+                    <Heading size={"small"}>
+                        Versjon 23.11 - November 2023
+                    </Heading>
+                    <BodyLong>
+                        {NOVEMBER23}
+                    </BodyLong>
+                </VStack>
+            </Box>
+        <Contact/>
+        </SupportTemplate>
+    )
+}
+
+export default Version;
---
+++ src/components/utils/version/VersionTexts.ts
@@ -0,0 +1,9 @@
+export const ABOUT_VERSIONS: string[] = [
+    "FLYT er under kontinuerlig utvikling og det blir stadig lagt til ny funksjonalitet samt forbedret brukeropplevelse basert på brukernes innspill - Denne siden beskriver og " +
+    "viser større endringer i FLYT men feilrettinger vil også bli beskrevet på denne siden"
+]
+
+
+export const NOVEMBER23: string[] = [
+"23.11 versjonen inneholder diverse feilrettinger og noe endring av brukergrensesnittet"
+]
