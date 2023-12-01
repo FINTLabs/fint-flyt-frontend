c353c2c43d213baf29c153e70a4022a7577f66ec
Hans <51415032+hansodeg@users.noreply.github.com>
2023-12-01 14:54:19 +0100

    Ffs 566 fredag (#205)
    * flytta FlytTitleComponent.tsx høyere opp og refaktorerte
    
    * lagde egen komponent for å kunne kontakte Jon Erik
    
    * La til Version.tsx i routes
    
    * splittet opp Supportsiden for å gjenbruk av komponenter
    
    * La til versjons-side med tilhørende text
    
    * Fjerna aboutversions
    
    * retta semantikk
    
    * endra på overskrift og la på Knappetekst
    
    * bytte navn til engelsk version
    
    * bytta til samme box
    
    * bytta til samme box
    
    * fiksa indentering
    
    * fjerna whitespace
    
    * litt mer indentering
    
    * Bytta navn til InformationTemplate.tsx og refaktorerte
    
    * Endra til version i menu
    
    * fjerna kontaktinfo fra versjonssiden
    
    * La til Listview på Version.tsx (takk Ingrid)
    
    * ta til test av version side
    
    * fjerna ubrukt const og la til engelsk tekst
    
    * la til versjon for Desember
    
    * oppdaterte tester
    
    * oppdaterte tester
    
    * fjerna spor av server i dockerFile

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,12 @@ COPY . /src
 RUN yarn test:ci && yarn build
 
 
+FROM node:16
+WORKDIR /usr/src/app
+RUN mkdir -p server
+COPY server/package*.json server
+COPY server/yarn.lock*.json server
+COPY --from=builder /src/build/ build
+RUN yarn --cwd server install
+COPY server server
+CMD [ "node", "server/index.js" ]
--- src/components/utils/version/VersionTexts.ts
+++ src/components/utils/version/VersionTexts.ts
@@ -3,4 +3,15 @@ export const ABOUT_VERSIONS: string[] = [
     "større endringer og feilrettinger i FLYT"
 ]
 
-export const VERSION_DATA: {heading: string, updates: string[]}[] = [ { heading: "Versjon 23.12 - November 2023", updates: [ "Lagt til ny side som beskriver gjeldene versjon av FLYT" ] } ,{ heading: "Versjon 23.11 - November 2023", updates: [ "23.11 versjonen inneholder diverse feilrettinger og noe endring av brukergrensesnittet" ] } ]
+export const VERSION_DATA: {heading: string, updates: string[]}[] = [ { heading: "Versjon 1.12 - November 2023",
+    updates: [
+        "1.12 La til ny side med visning av versjoner og hva som er nytt i Flyt"
+    ]
+},
+    { heading: "Versjon 30.11 - November 2023",
+        updates: [
+            "30.11  Nytt design av vertikonvertering siden og oppdaterete feilmeldinger for instans som feilet ved innsending",
+            "27.11  Nytt design av support siden",
+            "23.11  Diverse feilrettinger og noe endring av brukergrensesnittet"
+        ]
+    } ]
