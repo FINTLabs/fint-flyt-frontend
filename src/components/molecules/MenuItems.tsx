42fea5ddf49f62aabc511e90ae25aab808c54672
hansodeg <hansodeg@gmail.com>
2023-12-05 23:31:27 +0100

    FLytta menuitems til /molecules

--- src/components/molecules/MenuItems.tsx
+++ src/components/molecules/MenuItems.tsx
@@ -1,5 +1,5 @@
 import { useHistory } from "react-router-dom";
-import routes from "./Routes";
+import routes from "../../features/main/Routes";
 import { useTranslation } from "react-i18next";
 import { Button } from "@navikt/ds-react";
 
--- src/components/organisms/AppBar.tsx
+++ src/components/organisms/AppBar.tsx
@@ -1,5 +1,5 @@
 import { Link as RouterLink } from "react-router-dom";
-import MenuItems from "../../features/main/MenuItems";
+import MenuItems from "../molecules/MenuItems";
 import { InternalHeader, Button } from "@navikt/ds-react";
 import React from "react";
 
