21db310b46d1d19a827dbb8a6b3bb2567304715c
hansodeg <hansodeg@gmail.com>
2023-12-05 23:20:24 +0100

    la på ny farge

--- src/features/main/MenuItems.tsx
+++ src/features/main/MenuItems.tsx
@@ -1,18 +1,18 @@
-import React from "react";
 import { useHistory } from "react-router-dom";
 import routes from "./Routes";
 import { useTranslation } from "react-i18next";
-import { Button, ToggleGroup } from "@navikt/ds-react";
+import { Button } from "@navikt/ds-react";
 const MenuItems = () => {
 	const { t } = useTranslation("translations", { keyPrefix: "menuItems" });
 	const history = useHistory();
 	return (
-		<ToggleGroup onChange={() => React.Children}>
+		<>
 			{routes
 				.filter((route) => route.inNavigationMenu)
 				.map((route) => (
 					<Button
-						size={"small"}
+						size={"medium"}
+						style={{ color: "white" }}
 						variant={"tertiary-neutral"}
 						onClick={() => {
 							history.push(route.path);
@@ -22,7 +22,7 @@ const MenuItems = () => {
 						{t(route.name)}
 					</Button>
 				))}
-		</ToggleGroup>
+		</>
 	);
 };
 export default MenuItems;
