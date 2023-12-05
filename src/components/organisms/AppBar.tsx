ba41416e06af079b3e5c328913be72c77be57c73
hansodeg <hansodeg@gmail.com>
2023-12-05 22:28:34 +0100

    Fjerna alle spor av MUI. bruker router hook for bytte av side

--- src/components/organisms/AppBar.tsx
+++ src/components/organisms/AppBar.tsx
@@ -1,20 +1,15 @@
-import {Button, Toolbar} from "@mui/material";
 import {Link as RouterLink} from "react-router-dom";
 import MenuItems from "../../features/main/MenuItems";
-import {Box} from "@navikt/ds-react";
+import { InternalHeader,  Button} from "@navikt/ds-react";
 import React from "react";
 
 export const AppBar = () => {
-    return <Box style={{backgroundColor: "#1F4F59"}}>
-        <Toolbar id={"toolbar"}>
-            <Button component={RouterLink} to="/">
-                <img src="https://cdn.flais.io/media/fint-by-vigo-white.svg" alt="logo"
-                     style={{width: 80, marginRight: '32px'}}/>
-            </Button>
-            <nav>
-                <MenuItems/>
-            </nav>
-        </Toolbar>
-    </Box>
+    return (
+        <InternalHeader style={{backgroundColor: "#1F4F59"}} >
+           <Button  variant="tertiary-neutral" icon={<img src="https://cdn.flais.io/media/fint-by-vigo-white.svg" alt="logo" style={{width: 80, marginRight: '32px'}}/>} component={RouterLink} to="/"/>
+           <MenuItems/>
+        </InternalHeader>
+
+    )
 }
 
