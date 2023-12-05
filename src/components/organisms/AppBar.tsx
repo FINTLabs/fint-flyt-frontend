f3e0607a057c9269e194153bd1b2c5dcf01c847e
hansodeg <hansodeg@gmail.com>
2023-12-05 22:44:16 +0100

    indentering

--- src/components/organisms/AppBar.tsx
+++ src/components/organisms/AppBar.tsx
@@ -1,15 +1,24 @@
-import {Link as RouterLink} from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import MenuItems from "../../features/main/MenuItems";
-import { InternalHeader,  Button} from "@navikt/ds-react";
+import { InternalHeader, Button } from "@navikt/ds-react";
 import React from "react";
 
 export const AppBar = () => {
-    return (
-        <InternalHeader style={{backgroundColor: "#1F4F59"}} >
-           <Button  variant="tertiary-neutral" icon={<img src="https://cdn.flais.io/media/fint-by-vigo-white.svg" alt="logo" style={{width: 80, marginRight: '32px'}}/>} component={RouterLink} to="/"/>
-           <MenuItems/>
-        </InternalHeader>
-
-    )
-}
-
+	return (
+		<InternalHeader style={{ backgroundColor: "#1F4F59" }}>
+			<Button
+				variant="tertiary-neutral"
+				icon={
+					<img
+						src="https://cdn.flais.io/media/fint-by-vigo-white.svg"
+						alt="logo"
+						style={{ width: 80, marginRight: "32px" }}
+					/>
+				}
+				component={RouterLink}
+				to="/"
+			/>
+			<MenuItems />
+		</InternalHeader>
+	);
+};
--- src/features/main/MenuItems.tsx
+++ src/features/main/MenuItems.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import routes from './Routes';
-import { useTranslation } from 'react-i18next';
-import { Button, ToggleGroup } from '@navikt/ds-react';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import routes from "./Routes";
+import { useTranslation } from "react-i18next";
+import { Button, ToggleGroup } from "@navikt/ds-react";
 const MenuItems = () => {
-    const { t } = useTranslation('translations', { keyPrefix: 'menuItems' });
-    const history = useHistory();
-    return (
-        <ToggleGroup onChange={() => React.Children} >
-            {routes
-                .filter(route => route.inNavigationMenu)
-                .map((route) => (
-                    <Button size={"small"}
-                        variant={'tertiary-neutral'}
-                        onClick={() => {
-                            history.push(route.path);
-                        }}
-                        key={route.name}>
-                        {t(route.name)}
-                    </Button>
-                ))}
-        </ToggleGroup>
-    );
+	const { t } = useTranslation("translations", { keyPrefix: "menuItems" });
+	const history = useHistory();
+	return (
+		<ToggleGroup onChange={() => React.Children}>
+			{routes
+				.filter((route) => route.inNavigationMenu)
+				.map((route) => (
+					<Button
+						size={"small"}
+						variant={"tertiary-neutral"}
+						onClick={() => {
+							history.push(route.path);
+						}}
+						key={route.name}
+					>
+						{t(route.name)}
+					</Button>
+				))}
+		</ToggleGroup>
+	);
 };
 export default MenuItems;
