1cc92972ae7fa36a1e8778d12fa8a0ddbbf04bb9
ingridevensen <ingride89@gmail.com>
2023-12-07 13:39:37 +0100

    add toggle group

--- src/features/valueConverting/components/ValueConvertingTable.tsx
+++ src/features/valueConverting/components/ValueConvertingTable.tsx
@@ -12,6 +12,7 @@ import {
     HStack,
     Pagination,
     Table,
+    ToggleGroup,
     VStack
 } from "@navikt/ds-react";
 import {MenuElipsisVerticalCircleIcon} from "@navikt/aksel-icons";
@@ -28,26 +29,35 @@ const ValueConvertingTable: React.FunctionComponent<Props> = (props: Props) => {
     const [rows, setRows] = useState<IValueConverting[] | undefined>(undefined)
     const [page, setPage] = useState(1);
     const rowsPerPage = 8;
+    const [toggleValue, setToggleValue] = useState<string>('manual')
 
     let sortData = rows ?? [];
     sortData = sortData.slice((page - 1) * rowsPerPage, page * rowsPerPage);
 
 
     useEffect(() => {
-        ValueConvertingRepository.getValueConvertings(0, 100, 'id', 'DESC', false)
-            .then(response => {
-                const data = response.data
-                if (data.content) {
-                    setRows(data.content)
-                } else {
+        toggleValue === 'manual' ?
+            ValueConvertingRepository.getValueConvertings(0, 100, 'id', 'DESC', false)
+                .then(response => {
+                    const data = response.data
+                    if (data.content) {
+                        setRows(data.content)
+                    } else {
+                        setRows([])
+                    }
+                })
+                .catch(e => {
+                    console.log(e)
                     setRows([])
-                }
-            })
-            .catch(e => {
-                console.log(e)
-                setRows([])
-            })
-    }, [])
+                }) : setRows([
+                {convertingMap: {ac3: "https://beta.felleskomponent.no/arkiv/kodeverk/format/systemid/AC3"},
+                    displayName:"Til stor forbokstav",
+                    fromApplicationId:2,
+                    fromTypeId:"tekst",
+                    id:6,
+                    toApplicationId:"fylkesrad",
+                    toTypeId:"tekst"}])
+    }, [toggleValue, setToggleValue])
 
     async function handleNewOrEditConvertingClick(id: number) {
         props.onValueConvertingSelected(id)
@@ -72,59 +82,72 @@ const ValueConvertingTable: React.FunctionComponent<Props> = (props: Props) => {
     }
 
     return (
-            <Box background={"surface-default"} padding="6" borderRadius={"large"} borderWidth="2" borderColor={"border-subtle"}>
-                <VStack gap={"6"}>
-                    <Box background={'surface-default'} style={{height: '490px', overflowY: "scroll"}}>
-                        <Table id={"value-convertings-table"} size={"small"}>
-                            <Table.Header>
-                                <Table.Row>
-                                    <Table.HeaderCell scope="col">{t('column.show')}</Table.HeaderCell>
-                                    <Table.HeaderCell scope="col">{t('column.displayName')}</Table.HeaderCell>
-                                    <Table.HeaderCell scope="col">{t('column.fromType')}</Table.HeaderCell>
-                                    <Table.HeaderCell scope="col">{t('column.toType')}</Table.HeaderCell>
-                                    <Table.HeaderCell scope="col">{t('column.fromApplication')}</Table.HeaderCell>
-                                    <Table.HeaderCell scope="col">{t('column.toApplication')}</Table.HeaderCell>
-                                    <Table.HeaderCell scope="col">{t('column.actions')}</Table.HeaderCell>
-                                </Table.Row>
-                            </Table.Header>
-                            <Table.Body>
-                                {sortData?.map((value, i) => {
-                                    return (
-                                        <Table.ExpandableRow id={"table-row-" + i} key={i}
-                                                             content={<ValueConvertingPanel existingValueConverting={value}/>}>
-                                            <Table.DataCell scope="row">{value.displayName}</Table.DataCell>
-                                            <Table.DataCell scope="row">{value.fromTypeId}</Table.DataCell>
-                                            <Table.DataCell scope="row">{value.toTypeId}</Table.DataCell>
-                                            <Table.DataCell scope="row">{getSourceApplicationDisplayName(value.fromApplicationId)}</Table.DataCell>
-                                            <Table.DataCell scope="row">{getDestinationDisplayName(value.toApplicationId)}</Table.DataCell>
-                                            <Table.DataCell scope="row">
-                                                {actionMenu(value)}
-                                            </Table.DataCell>
-                                        </Table.ExpandableRow>
-                                    );
-                                })}
-                            </Table.Body>
-                        </Table>
-                    </Box>
-                    <HStack justify={"center"}>
-                        {rows && rows.length > rowsPerPage &&
-                            <Pagination
-                                page={page}
-                                onPageChange={setPage}
-                                count={Math.ceil(rows.length / rowsPerPage)}
-                                size="small"
-                            />}
-                    </HStack>
-                    <HStack gap={"2"} align="center">
-                        <ButtonAks id={"new-button"} onClick={() => props.setNewValueConverting(true)}>
-                            {t('button.newConverting')}
-                        </ButtonAks>
-                        <HelpText title="Knapp informasjon" placement="right">
-                            {t('help.new')}
-                        </HelpText>
-                    </HStack>
-                </VStack>
-            </Box>
+        <Box background={"surface-default"} padding="6" borderRadius={"large"} borderWidth="2"
+             borderColor={"border-subtle"}>
+            <VStack gap={"6"}>
+                <Box style={{alignSelf: "center"}}>
+                    <ToggleGroup defaultValue="manual" onChange={(value) => {
+                        setRows([])
+                        setToggleValue(value)
+                        }
+                    } size={"medium"}>
+                        <ToggleGroup.Item value="manual">Manuelle</ToggleGroup.Item>
+                        <ToggleGroup.Item value="flyt">Flyt</ToggleGroup.Item>
+                        <ToggleGroup.Item value="application">Applikasjon/Destinasjon</ToggleGroup.Item>
+                    </ToggleGroup>
+                </Box>
+                <Box background={'surface-default'} style={{height: '490px', overflowY: "scroll"}}>
+                    <Table id={"value-convertings-table"} size={"small"}>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell scope="col">{toggleValue === 'manual' ? t('column.show') : ''}</Table.HeaderCell>
+                                <Table.HeaderCell scope="col">{t('column.id')}</Table.HeaderCell>
+                                <Table.HeaderCell scope="col">{t('column.displayName')}</Table.HeaderCell>
+                                <Table.HeaderCell scope="col">{t('column.fromType')}</Table.HeaderCell>
+                                <Table.HeaderCell scope="col">{t('column.toType')}</Table.HeaderCell>
+                                {toggleValue === 'manual' && <Table.HeaderCell scope="col">{t('column.fromApplication')}</Table.HeaderCell>}
+                                {toggleValue === 'manual' && <Table.HeaderCell scope="col">{t('column.toApplication')}</Table.HeaderCell>}
+                                {toggleValue === 'manual' && <Table.HeaderCell scope="col">{t('column.actions')}</Table.HeaderCell>}
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {sortData?.map((value, i) => {
+                                return (
+                                    <Table.ExpandableRow id={"table-row-" + i} key={i} expansionDisabled={toggleValue !== 'manual'}
+                                                         content={toggleValue === 'manual' ? <ValueConvertingPanel
+                                                             existingValueConverting={value}/> : <></>}>
+                                        <Table.DataCell scope="row">{value.id}</Table.DataCell>
+                                        <Table.DataCell scope="row">{value.displayName}</Table.DataCell>
+                                        <Table.DataCell scope="row">{value.fromTypeId}</Table.DataCell>
+                                        <Table.DataCell scope="row">{value.toTypeId}</Table.DataCell>
+                                        {toggleValue === 'manual' && <Table.DataCell scope="row">{getSourceApplicationDisplayName(value.fromApplicationId)}</Table.DataCell>}
+                                        {toggleValue === 'manual' && <Table.DataCell scope="row">{getDestinationDisplayName(value.toApplicationId)}</Table.DataCell>}
+                                        {toggleValue === 'manual' && <Table.DataCell scope="row">{actionMenu(value)}</Table.DataCell>}
+                                    </Table.ExpandableRow>
+                                );
+                            })}
+                        </Table.Body>
+                    </Table>
+                </Box>
+                <HStack justify={"center"}>
+                    {rows && rows.length > rowsPerPage &&
+                        <Pagination
+                            page={page}
+                            onPageChange={setPage}
+                            count={Math.ceil(rows.length / rowsPerPage)}
+                            size="small"
+                        />}
+                </HStack>
+                {toggleValue === 'manual' && <HStack gap={"2"} align="center">
+                    <ButtonAks id={"new-button"} onClick={() => props.setNewValueConverting(true)}>
+                        {t('button.newConverting')}
+                    </ButtonAks>
+                    <HelpText title="Knapp informasjon" placement="right">
+                        {t('help.new')}
+                    </HelpText>
+                </HStack>}
+            </VStack>
+        </Box>
     );
 }
 
