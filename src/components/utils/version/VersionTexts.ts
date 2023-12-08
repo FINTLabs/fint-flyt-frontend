1f091988cab4529a61feba356096bf781c3e1c35
ingridevensen <ingride89@gmail.com>
2023-12-08 11:35:08 +0100

    add news to version data

--- src/components/utils/version/VersionTexts.ts
+++ src/components/utils/version/VersionTexts.ts
@@ -3,12 +3,14 @@ export const ABOUT_VERSIONS: string[] = [
     "større endringer og feilrettinger i FLYT"
 ]
 
-export const VERSION_DATA: {heading: string, updates: string[]}[] = [ { heading: "Versjon 1.12 - Desember 2023",
-    updates: [
-        "1.12 La til ny side med visning av versjoner og hva som er nytt i Flyt"
-    ]
-},
-    { heading: "Versjon 30.11 - November 2023",
+export const VERSION_DATA: {heading: string, updates: string[]}[] = [
+    { heading: "Versjon 8.12 - Desember 2023",
+        updates: [
+            "8.12 Forbedret visning av verdikonvertering med flere verdier. Nytt design av instanssiden hvor all informasjon er samlet på én flate.",
+            "1.12 La til ny side med visning av versjoner og hva som er nytt i Flyt"
+        ]
+    },
+    { heading: "November 2023",
         updates: [
             "30.11  Nytt design av vertikonvertering siden og oppdaterete feilmeldinger for instans som feilet ved innsending",
             "27.11  Nytt design av support siden",
