e9f70ee3a208356ae9f729341d173f0d1ea9a307
ingridevensen <ingride89@gmail.com>
2023-12-08 14:14:01 +0100

    FFS-642 add i18n translations

--- src/components/pages/Version.tsx
+++ src/components/pages/Version.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {BodyLong, Box, List, VStack} from "@navikt/ds-react";
 import PageTemplate from "../templates/PageTemplate";
-import {ABOUT_VERSIONS, VERSION_DATA} from "../utils/version/VersionTexts";
+import {ABOUT_VERSIONS, ABOUT_VERSIONS_NN, VERSION_DATA, VERSION_DATA_NN} from "../utils/version/VersionTexts";
 import {RouteComponent} from "../../routes/Route";
+import {useTranslation} from "react-i18next";
 
 const Version: RouteComponent = () => {
+    const {i18n} = useTranslation();
+
     return (
         <PageTemplate id={'version'} keyPrefix={'pages.version'}>
             <Box
@@ -16,10 +19,10 @@ const Version: RouteComponent = () => {
                 borderColor={"border-subtle"}
             >
                 <VStack gap={"6"}>
-                    <BodyLong>{ABOUT_VERSIONS}</BodyLong>
+                    <BodyLong>{i18n.language === 'no' ? ABOUT_VERSIONS : ABOUT_VERSIONS_NN}</BodyLong>
                     <BodyLong>
                         <VStack gap={"6"}>
-                            {VERSION_DATA.map((value, i) => (
+                            {(i18n.language === 'no' ? VERSION_DATA : VERSION_DATA_NN).map((value, i) => (
                                 <List key={i} as="ul" title={value.heading}>
                                     {value.updates.map((update, i) => (
                                         <List.Item key={i}>{update}</List.Item>
