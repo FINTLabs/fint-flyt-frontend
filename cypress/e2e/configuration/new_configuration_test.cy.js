f24386576491cd10a32dbf50001b9bc1b4b10a34
ingridevensen <ingride89@gmail.com>
2023-12-11 21:07:15 +0100

    include all tests

--- cypress/e2e/configuration/new_configuration_test.cy.js
+++ cypress/e2e/configuration/new_configuration_test.cy.js
@@ -66,7 +66,7 @@ describe('Testing create new configuration from new integration', () => {
     })
 });
 
-describe.skip('Testing creating new and editing configurations from integration overview', () => {
+describe('Testing creating new and editing configurations from integration overview', () => {
     function prep() {
         cy.intercept('GET', '**/api/application/configuration', {
             forceNetworkError: true,
@@ -78,44 +78,46 @@ describe.skip('Testing creating new and editing configurations from integration 
 
     beforeEach(() => {
         cy.intercept('POST', '**/integrasjoner', {fixture: 'postFixture.json'}).as('postIntegration');
-        cy.intercept('GET', '**/integrasjoner?side=0&sorteringFelt=state&sorteringRetning=ASC', {fixture: 'integrations.json'}).as('getIntegrations');
+        cy.intercept('GET', '**/integrasjoner?side=0&sorteringFelt=state&sorteringRetning=ASC', {fixture: 'integrationsInList.json'}).as('getIntegrations');
         cy.intercept('GET', '**/integrasjoner', {fixture: 'allIntegrations.json'}).as('getAllIntegrations');
-        cy.intercept('GET', '**/historikk/statistikk/integrasjoner', {fixture: 'historikk.json'}).as('getHistory');
+        cy.intercept('GET', '**/historikk/statistikk/integrasjoner', {fixture: 'historikk.json'}).as('getHistory')
         cy.intercept('GET', '**/metadata?kildeapplikasjonId=*&bareSisteVersjoner=true', {fixture: 'metadataLatest.json'}).as('getLatestMetadata');
-        cy.intercept('GET', '**/metadata?kildeapplikasjonId=*&bareSisteVersjoner=false', {fixture: 'metadata.json'}).as('getMetadata');
+        cy.intercept('GET', '**/metadata?kildeapplikasjonId=*&bareSisteVersjoner=*', {fixture: 'metadata.json'}).as('getMetadata')
         cy.intercept('GET', '**/metadata/11/instans-metadata', {fixture: 'instansMetadata.json'}).as('getInstansMetadata');
         cy.intercept('GET', '**/metadata/1/instans-metadata', {fixture: 'instansMetadata.json'}).as('getInstansMetadata');
         cy.intercept('GET', '**/value-convertings?page=0&size=100&sortProperty=fromApplicationId&sortDirection=ASC&excludeConvertingMap=false', {fixture: 'valueconverting/valueconvertings.json'}).as('getValueconvertings');
         cy.intercept('GET', '**/arkiv/kodeverk/**', {fixture: 'kodeverk/mock.json'}).as('getKodeverk');
-        cy.intercept('GET', '**/konfigurasjoner/?side=0&antall=10000&sorteringFelt=version&sorteringRetning=DESC&ferdigstilt=false&integrasjonId=2&ekskluderMapping=true', {fixture: 'configDrafts.json'}).as('getConfigDrafts');
-        cy.intercept('GET', '**/konfigurasjoner/?side=0&antall=10000&sorteringFelt=id&sorteringRetning=ASC&ferdigstilt=true&integrasjonId=2&ekskluderMapping=true', {fixture: 'configCompleted.json'}).as('getConfigCompleted');
+        cy.intercept('GET', '**/konfigurasjoner/?side=0&antall=10000&sorteringFelt=id&sorteringRetning=DESC&ferdigstilt=false&integrasjonId=1&ekskluderMapping=true', {fixture: 'configDrafts.json'}).as('getConfigDrafts');
+        cy.intercept('GET', '**/konfigurasjoner/?side=0&antall=10000&sorteringFelt=id&sorteringRetning=DESC&ferdigstilt=false&integrasjonId=2&ekskluderMapping=true', {fixture: 'configDrafts2.json'}).as('getConfigDrafts2');
+        cy.intercept('GET', '**/konfigurasjoner/?side=0&antall=10000&sorteringFelt=version&sorteringRetning=DESC&ferdigstilt=true&integrasjonId=1&ekskluderMapping=true', {fixture: 'configCompleted.json'}).as('getConfigCompleted');
+        cy.intercept('GET', '**/konfigurasjoner/?side=0&antall=10000&sorteringFelt=version&sorteringRetning=DESC&ferdigstilt=true&integrasjonId=2&ekskluderMapping=true', {fixture: 'configCompleted2.json'}).as('getConfigCompleted2');
         cy.intercept('GET', '**/konfigurasjoner/5?ekskluderMapping=false', {fixture: 'config.json'}).as('getConfig');
+        cy.intercept('GET', '**/konfigurasjoner/4?ekskluderMapping=true', {fixture: 'config.json'}).as('getConfig');
         cy.intercept('GET', '**/konfigurasjoner/6?ekskluderMapping=false', {fixture: 'editConfig.json'}).as('getEditConfig');
     });
 
     it('should navigate to create new blank configuration form', () => {
         prep()
-        cy.get('[data-id="journalpost"] > [data-field="details"] > .MuiButtonBase-root').click()
-        cy.get('#positioned-button').click()
-        cy.get('#new-configuration-button').click()
+        cy.get(':nth-child(3) > .navds-table__toggle-expand-cell > .navds-table__toggle-expand-button').click()
+        cy.get('#rb > .navds-table__expanded-row-cell > .navds-table__expanded-row-collapse > .navds-table__expanded-row-content > #integration-panel-container > .navds-vstack > .navds-stack > .navds-box > #new-configuration-button').click()
     })
 
     it('should navigate to create new configuration based on existing completed version', () => {
         prep()
-        cy.get('[data-id="journalpost"] > [data-field="details"] > .MuiButtonBase-root').click()
-        cy.get('#positioned-button').click()
-        cy.get('li.MuiButtonBase-root > #positioned-button').click()
-        cy.get('#version-button').click()
-        cy.get('#mapping\\.valueMappingPerKey\\.type\\.mappingString').click()
-        cy.get('#menu-mapping\\.valueMappingPerKey\\.type\\.mappingString > .MuiPaper-root > .MuiList-root > [tabindex="0"]').type('{enter}')
-        cy.get('.MuiToggleButton-root').click()
-        cy.get('#dnd-value-component-mapping\\.objectMappingPerKey\\.newCase\\.valueMappingPerKey\\.tittel\\.mappingString > .MuiFormControl-root > .MuiInputBase-root')
+        cy.get(':nth-child(3) > .navds-table__toggle-expand-cell > .navds-table__toggle-expand-button').click()
+        cy.get('.min-h-32 > .navds-dropdown__toggle').click()
+        cy.get('.min-h-32 > .navds-popover > dl.navds-dropdown__list > :nth-child(2) > .navds-dropdown__item').click()
+
+      cy.get('#mapping\\.valueMappingPerKey\\.type\\.mappingString').click()
+     cy.get('#menu-mapping\\.valueMappingPerKey\\.type\\.mappingString > .MuiPaper-root > .MuiList-root > [tabindex="0"]').type('{enter}')
+      cy.get('.MuiToggleButton-root').click()
+      cy.get('#dnd-value-component-mapping\\.objectMappingPerKey\\.newCase\\.valueMappingPerKey\\.tittel\\.mappingString > .MuiFormControl-root > .MuiInputBase-root').should("contain.text", "test basert på")
     })
 
     it('should navigate to edit existing configuration draft', () => {
         prep()
-        cy.get('[data-id="journalpost"] > [data-field="details"] > .MuiButtonBase-root').click()
-        cy.get('[data-id="6"] > .MuiDataGrid-cell--withRenderer > .MuiButtonBase-root').click()
-        cy.get('#comment').should("contain.text", "rediger denne konfig")
+        cy.get(':nth-child(3) > .navds-table__toggle-expand-cell > .navds-table__toggle-expand-button').click()
+            cy.get(':nth-child(2) > :nth-child(3) > .min-h-32 > .navds-button').click()
+       cy.get('#comment').should("contain.text", "rediger denne konfig")
     })
 });
