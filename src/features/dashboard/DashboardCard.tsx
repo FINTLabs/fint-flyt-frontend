56f87000da1d977fbdfccd2b68ee0446578c47ef
hansodeg <hansodeg@gmail.com>
2023-12-28 15:52:02 +0100

    Fjerna mui fra dashboardcard

--- src/features/dashboard/DashboardCard.tsx
+++ src/features/dashboard/DashboardCard.tsx
@@ -1,9 +1,6 @@
-import { Button, CardActions, CardContent, Divider } from "@mui/material";
 import * as React from "react";
-import { Link as RouterLink } from "react-router-dom";
 import { ILink } from "./types/Link";
-
-import { Box, Heading, HelpText, HStack, Loader } from "@navikt/ds-react";
+import { Box, Heading, BodyLong, Link } from "@navikt/ds-react";
 
 type Props = {
 	content: string;
@@ -13,39 +10,25 @@ type Props = {
 };
 
 const DashboardCard: React.FunctionComponent<Props> = (props: Props) => {
-	const [isHovered, setHovered] = React.useState(false);
-
 	return (
 		<Box
 			id={"support-information"}
 			background={"surface-default"}
 			padding="6"
 			borderRadius={"large"}
-			shadow="medium"
+			shadow="small"
 			borderWidth="2"
 			borderColor={"border-subtle"}
-			onMouseOver={() => setHovered(true)}
-			onMouseOut={() => setHovered(false)}
 		>
-			<CardContent>
-				<Heading size="medium"> {props.value}</Heading>
-				<Heading size="small">{props.content}</Heading>
-			</CardContent>
-			<CardActions>
-				{props.links &&
-					props.links.map((link: ILink, index: number) => (
-						<Button
-							key={index}
-							id={props.id + `-btn-` + index}
-							size="small"
-							variant="text"
-							component={RouterLink}
-							to={link.href}
-						>
-							<Heading size="small">{link.name}</Heading>
-						</Button>
-					))}
-			</CardActions>
+			<Heading size="medium"> {props.value}</Heading>
+			<Heading size="small">{props.content}</Heading>
+
+			{props.links &&
+				props.links.map((link: ILink, index: number) => (
+					<BodyLong key={index} id={props.id + `-btn-` + index}>
+						<Link href={link.href}>{link.name}</Link>
+					</BodyLong>
+				))}
 		</Box>
 	);
 };
