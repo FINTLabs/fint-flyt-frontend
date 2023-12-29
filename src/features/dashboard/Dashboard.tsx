46f9c2e624f13798747c4228c8bffa8ae9c4ff80
hansodeg <hansodeg@gmail.com>
2023-12-29 10:39:00 +0100

    lagde ny fil - support content og importerer i Dash

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -6,19 +6,10 @@ import { useTranslation } from "react-i18next";
 import { IIntegrationStatistics } from "./types/IntegrationStatistics";
 import PageTemplate from "../../components/templates/PageTemplate";
 import { RouteComponent } from "../../routes/Route";
-import {
-	BodyLong,
-	Box,
-	ExpansionCard,
-	HStack,
-	Heading,
-	Link,
-	List,
-	VStack,
-} from "@navikt/ds-react";
-import { Link as RouterLink } from "react-router-dom";
-import { FAQ, FLYT_DESCRIPTION } from "../support/util/supportTexts";
+import { HStack } from "@navikt/ds-react";
+
 import { Contact } from "../../components/atoms/Contact";
+import SupportContent from "../../components/molecules/SupportContent";
 
 const Dashboard: RouteComponent = () => {
 	const { t } = useTranslation("translations", {
@@ -92,47 +83,7 @@ const Dashboard: RouteComponent = () => {
 					);
 				})}
 			</HStack>
-			<Box
-				id={"support-information"}
-				background={"surface-default"}
-				padding="6"
-				borderRadius={"large"}
-				borderWidth="2"
-				borderColor={"border-subtle"}
-			>
-				<VStack gap={"6"}>
-					<Heading size={"small"}>Hva er FINT Flyt?</Heading>
-					<BodyLong>{FLYT_DESCRIPTION}</BodyLong>
-					<BodyLong>
-						Du kan se en brukerveiledning til hvordan du bruker Fint Flyt{" "}
-						<Link
-							as={RouterLink}
-							id={"support-guide-link"}
-							to={"/support/guide"}
-						>
-							her
-						</Link>
-					</BodyLong>
-				</VStack>
-			</Box>
-			<Box id={"support-faq"}>
-				<ExpansionCard aria-label="default">
-					<ExpansionCard.Header id={"support-faq-header"}>
-						<ExpansionCard.Title>Ofte stilte spørsmål</ExpansionCard.Title>
-					</ExpansionCard.Header>
-					<ExpansionCard.Content>
-						<List as="ul" id={"faq-list"}>
-							{FAQ.map((item, index) => {
-								return (
-									<List.Item key={index} title={item.header}>
-										{item.content}
-									</List.Item>
-								);
-							})}
-						</List>
-					</ExpansionCard.Content>
-				</ExpansionCard>
-			</Box>
+			<SupportContent />
 			<Contact />
 		</PageTemplate>
 	);
