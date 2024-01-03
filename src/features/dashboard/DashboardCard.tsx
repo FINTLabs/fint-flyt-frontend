60ae0f60136e894207a91d1c8fec2934c5b5fca9
hansodeg <hansodeg@gmail.com>
2024-01-03 21:19:36 +0100

    tst

--- src/features/dashboard/DashboardCard.tsx
+++ src/features/dashboard/DashboardCard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { ILink } from "./types/Link";
 import { Box, Heading, BodyLong, Link } from "@navikt/ds-react";
+import { minWidth } from "@mui/system";
 
 type Props = {
 	content: string;
@@ -12,24 +13,26 @@ type Props = {
 const DashboardCard: React.FunctionComponent<Props> = (props: Props) => {
 	return (
 		<Box
-			justify={"space-between"}
 			id={"support-information"}
 			background={"surface-default"}
+			align={"center"}
 			padding="6"
 			borderRadius={"large"}
 			shadow="small"
 			borderWidth="2"
 			borderColor={"border-subtle"}
+			shape={"large"}
 		>
-			<Heading size="medium"> {props.value}</Heading>
-			<Heading size="small">{props.content}</Heading>
-
-			{props.links &&
-				props.links.map((link: ILink, index: number) => (
-					<BodyLong key={index} id={props.id + `-btn-` + index}>
-						<Link href={link.href}>{link.name}</Link>
-					</BodyLong>
-				))}
+			<Box>
+				<Heading size="medium"> {props.value}</Heading>
+				<Heading size="small">{props.content}</Heading>
+				{props.links &&
+					props.links.map((link: ILink, index: number) => (
+						<BodyLong key={index} id={props.id + `-btn-` + index}>
+							<Link href={link.href}>{link.name}</Link>
+						</BodyLong>
+					))}
+			</Box>
 		</Box>
 	);
 };
