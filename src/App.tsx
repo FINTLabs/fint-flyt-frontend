89dd73268c8958930a47ca7e7206c70d765cf6b9
hansodeg <hansodeg@gmail.com>
2024-01-09 15:02:00 +0100

    installert tanstack Rquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,49 @@
-import React, {useEffect, useState} from 'react';
-import {ThemeProvider} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "@mui/material";
 import Main from "./components/pages/Main";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import IntegrationProvider from "./context/IntegrationContext";
 import SourceApplicationProvider from "./context/SourceApplicationContext";
 import axios from "axios";
-import HistoryProvider from './context/HistoryContext';
-import theme from './util/styles/theme/theme';
-import './global.css'
+import HistoryProvider from "./context/HistoryContext";
+import theme from "./util/styles/theme/theme";
+import "./global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-    const [basePath, setBasePath] = useState<string>();
-    useEffect(() => {
-        axios.get('api/application/configuration')
-            .then(value => {
-                axios.defaults.baseURL = value.data.basePath;
-                setBasePath(value.data.basePath);
-            })
-            .catch(reason => {
-                console.log(reason);
-                setBasePath('/');
-            })
-    }, [basePath]);
+	const [basePath, setBasePath] = useState<string>();
+	useEffect(() => {
+		axios
+			.get("api/application/configuration")
+			.then((value) => {
+				axios.defaults.baseURL = value.data.basePath;
+				setBasePath(value.data.basePath);
+			})
+			.catch((reason) => {
+				console.log(reason);
+				setBasePath("/");
+			});
+	}, [basePath]);
 
-    return basePath ?
-        (
-            <ThemeProvider theme={theme}>
-                <HistoryProvider>
-                    <SourceApplicationProvider>
-                        <IntegrationProvider>
-                            <BrowserRouter basename={basePath}>
-                                <Main/>
-                            </BrowserRouter>
-                        </IntegrationProvider>
-                    </SourceApplicationProvider>
-                </HistoryProvider>
-            </ThemeProvider>
-        )
-        : <h1>Loading</h1>
+	return basePath ? (
+		<ThemeProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<HistoryProvider>
+					<SourceApplicationProvider>
+						<IntegrationProvider>
+							<BrowserRouter basename={basePath}>
+								<Main />
+							</BrowserRouter>
+						</IntegrationProvider>
+					</SourceApplicationProvider>
+				</HistoryProvider>
+			</QueryClientProvider>
+		</ThemeProvider>
+	) : (
+		<h1>Loading</h1>
+	);
 }
 
 export default App;
