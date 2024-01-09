cf52b665400d16120fb1ef3a721003b4b9dc2ecc
ingridevensen <ingride89@gmail.com>
2024-01-09 16:14:01 +0100

    correct unit test

--- src/__tests__/mock/events.ts
+++ src/__tests__/mock/events.ts
@@ -1,49 +1,52 @@
 import {IEvent} from "../../features/instances/types/Event";
+import {Page} from "../../util/DataGridUtil";
 
-export const MOCK_EVENTS: IEvent[] = [
-    {
-        name: 'Event 1',
-        timeStamp: "2023-09-26T06:57:12.747Z",
-        type: 'Type 1',
-        errors: [],
-        instanceFlowHeaders: {
-            orgId: '52314',
-            service: 'flyt-dispatch-service',
-            archiveInstanceId: '5435',
-            sourceApplicationId: '2',
-            sourceApplicationIntegrationId: 'integrationId1',
-            sourceApplicationInstanceId: '6564',
-            correlationId: '54',
-            instanceId: '2',
-            configurationId: '3',
-            caseId: '33',
-            dispatchId: '213',
+export const MOCK_EVENTS: Page<IEvent> = {
+    content: [
+        {
+            name: 'Event 1',
+            timestamp: new Date("2023-09-26T06:57:12.747Z"),
+            type: 'Type 1',
+            errors: [],
+            instanceFlowHeaders: {
+                orgId: '52314',
+                service: 'flyt-dispatch-service',
+                archiveInstanceId: '5435',
+                sourceApplicationId: '2',
+                sourceApplicationIntegrationId: 'integrationId1',
+                sourceApplicationInstanceId: '6564',
+                correlationId: '54',
+                instanceId: '2',
+                configurationId: '3',
+                caseId: '33',
+                dispatchId: '213',
+            },
         },
-    },
-    {
-        name: 'Event 2',
-        timeStamp: "2023-09-26T06:57:12.747Z",
-        type: 'Type 2',
-        errors: [
-            {
-                errorCode: "FINT_FLYT_INSTANCE_GATEWAY_INSTANCE_REJECTED_ERROR",
-                args: {
-                    message: "error message'"
+        {
+            name: 'Event 2',
+            timestamp: new Date("2023-09-26T06:57:12.747Z"),
+            type: 'Type 2',
+            errors: [
+                {
+                    errorCode: "FINT_FLYT_INSTANCE_GATEWAY_INSTANCE_REJECTED_ERROR",
+                    args: {
+                        message: "error message'"
+                    }
                 }
-            }
-        ],
-        instanceFlowHeaders: {
-            orgId: '12345',
-            service: 'flyt-dispatch-service',
-            archiveInstanceId: '2345',
-            sourceApplicationId: '2',
-            sourceApplicationIntegrationId: 'sak',
-            sourceApplicationInstanceId: '123',
-            correlationId: '55',
-            instanceId: '2',
-            configurationId: '3',
-            caseId: '33',
-            dispatchId: '213',
-        },
-    },
-];
+            ],
+            instanceFlowHeaders: {
+                orgId: '12345',
+                service: 'flyt-dispatch-service',
+                archiveInstanceId: '2345',
+                sourceApplicationId: '2',
+                sourceApplicationIntegrationId: 'sak',
+                sourceApplicationInstanceId: '123',
+                correlationId: '55',
+                instanceId: '2',
+                configurationId: '3',
+                caseId: '33',
+                dispatchId: '213',
+            },
+        }
+    ]
+}
