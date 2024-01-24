dffbbcb2464ec990f9ed27e73c172a71584830e1
hansodeg <hansodeg@gmail.com>
2024-01-24 20:40:29 +0100

    la til ny hook useGetIntegrations

--- src/hooks/integration/useGetIntegrations.tsx
+++ src/hooks/integration/useGetIntegrations.tsx
@@ -2,14 +2,41 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 export const useGetAllIntegrations = () => {
 	const {
+		data: alleintegrasjoner,
+		error,
+		isFetched,
+	} = useQuery({
+		queryKey: ["alleintegrasjoner"],
+		queryFn: async () => {
+			return await axios.get("/api/intern/integrasjoner");
+		},
+	});
+	return { alleintegrasjoner, isFetched, error };
+};
+
+export const useGetIntegrations = (
+	page: number,
+	size: number | null,
+	sortProperty: string,
+	sortDirection: string
+) => {
+	const {
 		data: integrasjoner,
 		error,
 		isFetched,
 	} = useQuery({
 		queryKey: ["integrasjoner"],
 		queryFn: async () => {
-			return await axios.get("/api/intern/integrasjoner");
+			return await axios.get("/api/intern/integrasjoner", {
+				params: {
+					side: page,
+					antall: size,
+					sorteringFelt: sortProperty,
+					sorteringRetning: sortDirection,
+				},
+			});
 		},
 	});
+
 	return { integrasjoner, isFetched, error };
 };
