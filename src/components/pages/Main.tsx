130f10f2ce0bee74d14fd1b362a0cde9dba80011
ingridevensen <ingride89@gmail.com>
2024-03-05 10:07:57 +0100

    remove unused code

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useEffect} from "react";
 import ConfigurationProvider from "../../context/ConfigurationContext";
 import Router from "../../routes/Router";
 import {Box} from "@navikt/ds-react";
