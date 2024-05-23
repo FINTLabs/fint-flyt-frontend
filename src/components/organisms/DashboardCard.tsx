22707aabceb406d0bdde2896f57785154eb34149
ingridevensen <ingride89@gmail.com>
2024-05-23 09:37:00 +0200

    show skeleton on stats and info load in dashboard

--- src/components/organisms/DashboardCard.tsx
+++ src/components/organisms/DashboardCard.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { ILink } from "../../features/dashboard/Link";
-import { Box, Heading, BodyLong, Link } from "@navikt/ds-react";
+import {Box, Heading, BodyLong, Link, Skeleton} from "@navikt/ds-react";
 import { Link as RouterLink } from "react-router-dom";
+import {useContext} from "react";
+import {IntegrationContext} from "../../context/IntegrationContext";
 
 type Props = {
 	content: string;
@@ -11,6 +13,8 @@ type Props = {
 };
 
 const DashboardCard: React.FunctionComponent<Props> = (props: Props) => {
+	const { statistics, resetIntegrations, integrations, getAllIntegrations } = useContext(IntegrationContext);
+
 	return (
 		<Box
 			id={"support-information"}
@@ -21,16 +25,25 @@ const DashboardCard: React.FunctionComponent<Props> = (props: Props) => {
 			borderWidth="2"
 			borderColor={"border-subtle"}
 		>
-			<Heading size="medium"> {props.value}</Heading>
-			<Heading size="small">{props.content}</Heading>
-			{props.links &&
-				props.links.map((link: ILink, index: number) => (
-					<BodyLong key={index} id={props.id + `-btn-` + index}>
-						<Link as={RouterLink} to={link.href}>
-							{link.name}
-						</Link>
-					</BodyLong>
-				))}
+			{statistics && integrations ?
+				<>
+					<Heading size="medium"> {props.value}</Heading>
+					<Heading size="small">{props.content}</Heading>
+					{props.links &&
+						props.links.map((link: ILink, index: number) => (
+							<BodyLong key={index} id={props.id + `-btn-` + index}>
+								<Link as={RouterLink} to={link.href}>
+									{link.name}
+								</Link>
+							</BodyLong>
+						))}
+				</> :
+				<>
+					<Skeleton variant="text" width="25%" />
+					<Skeleton variant="text" width="100%" />
+					<Skeleton variant="text" width="100%" />
+				</>
+			}
 		</Box>
 	);
 };
