2ad88b8fdd744e0169ae7febd52449780996e2ce
Ingrid Evensen <ingrid@fintlabs.no>
2024-05-28 13:46:35 +0200

    add loader on fetching data (#276)

--- src/features/valueConverting/components/ValueConvertingTable.tsx
+++ src/features/valueConverting/components/ValueConvertingTable.tsx
@@ -9,7 +9,7 @@ import {
     Button as ButtonAks,
     Dropdown,
     HelpText,
-    HStack,
+    HStack, Loader,
     Pagination,
     Table,
     ToggleGroup,
@@ -81,73 +81,76 @@ const ValueConvertingTable: React.FunctionComponent<Props> = (props: Props) => {
     return (
         <Box background={"surface-default"} padding="6" borderRadius={"large"} borderWidth="2"
              borderColor={"border-subtle"}>
-            <VStack gap={"6"}>
-                {showToggle && <HStack style={{alignSelf: "center"}} gap={"3"} align={"center"} wrap={false}>
-                    <ToggleGroup defaultValue="custom" onChange={(value) => {
-                        setRows([])
-                        setToggleValue(value)
-                    }
-                    } size={"medium"}>
-                        <ToggleGroup.Item value="custom">{t('custom')}</ToggleGroup.Item>
-                        <ToggleGroup.Item value="flyt">{t('application')}</ToggleGroup.Item>
-                        <ToggleGroup.Item value="application">{t('destination')}</ToggleGroup.Item>
-                    </ToggleGroup>
-                    <HelpText title="Hva er dette?">
-                        {t('help.toggle')}
-                    </HelpText>
-                </HStack>}
-                <Box background={'surface-default'} style={{height: '490px', overflowY: "scroll"}}>
-                    <Table id={"value-convertings-table"} size={"small"}>
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell
-                                    scope="col">{toggleValue === 'custom' ? t('column.show') : ''}</Table.HeaderCell>
-                                <Table.HeaderCell scope="col">{t('column.id')}</Table.HeaderCell>
-                                <Table.HeaderCell scope="col">{t('column.displayName')}</Table.HeaderCell>
-                                <Table.HeaderCell scope="col">{t('column.fromType')}</Table.HeaderCell>
-                                <Table.HeaderCell scope="col">{t('column.toType')}</Table.HeaderCell>
-                                {toggleValue === 'custom' &&
-                                    <Table.HeaderCell scope="col">{t('column.fromApplication')}</Table.HeaderCell>}
-                                {toggleValue === 'custom' &&
-                                    <Table.HeaderCell scope="col">{t('column.toApplication')}</Table.HeaderCell>}
-                                {toggleValue === 'custom' &&
-                                    <Table.HeaderCell scope="col">{t('column.actions')}</Table.HeaderCell>}
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {sortData?.map((value, i) => {
-                                return (
-                                    <Table.ExpandableRow expandOnRowClick id={"table-row-" + i} key={i}
-                                                         expansionDisabled={toggleValue !== 'custom'}
-                                                         content={toggleValue === 'custom' ?
-                                                             <ValueConvertingPanel id={i}
-                                                                                   existingValueConverting={value}/> : <></>}>
-                                        <Table.DataCell scope="row">{value.id}</Table.DataCell>
-                                        <Table.DataCell scope="row">{value.displayName}</Table.DataCell>
-                                        <Table.DataCell scope="row">{value.fromTypeId}</Table.DataCell>
-                                        <Table.DataCell scope="row">{value.toTypeId}</Table.DataCell>
-                                        {toggleValue === 'custom' && <Table.DataCell
-                                            scope="row">{getSourceApplicationDisplayNameById(value.fromApplicationId, sourceApplications)}</Table.DataCell>}
-                                        {toggleValue === 'custom' && <Table.DataCell
-                                            scope="row">{getDestinationDisplayName(value.toApplicationId)}</Table.DataCell>}
-                                        {toggleValue === 'custom' &&
-                                            <Table.DataCell scope="row">{actionMenu(value)}</Table.DataCell>}
-                                    </Table.ExpandableRow>
-                                );
-                            })}
-                        </Table.Body>
-                    </Table>
-                </Box>
-                <HStack justify={"center"}>
-                    {rows && rows.length > rowsPerPage &&
-                        <Pagination
-                            page={page}
-                            onPageChange={setPage}
-                            count={Math.ceil(rows.length / rowsPerPage)}
-                            size="small"
-                        />}
-                </HStack>
-            </VStack>
+            {rows ?
+                <VStack gap={"6"}>
+                    {showToggle && <HStack style={{alignSelf: "center"}} gap={"3"} align={"center"} wrap={false}>
+                        <ToggleGroup defaultValue="custom" onChange={(value) => {
+                            setRows([])
+                            setToggleValue(value)}
+                        } size={"medium"}>
+                            <ToggleGroup.Item value="custom">{t('custom')}</ToggleGroup.Item>
+                            <ToggleGroup.Item value="flyt">{t('application')}</ToggleGroup.Item>
+                            <ToggleGroup.Item value="application">{t('destination')}</ToggleGroup.Item>
+                        </ToggleGroup>
+                        <HelpText title="Hva er dette?">
+                            {t('help.toggle')}
+                        </HelpText>
+                    </HStack>}
+                    <Box background={'surface-default'} style={{height: '490px', overflowY: "scroll"}}>
+                        <Table id={"value-convertings-table"} size={"small"}>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell
+                                        scope="col">{toggleValue === 'custom' ? t('column.show') : ''}</Table.HeaderCell>
+                                    <Table.HeaderCell scope="col">{t('column.id')}</Table.HeaderCell>
+                                    <Table.HeaderCell scope="col">{t('column.displayName')}</Table.HeaderCell>
+                                    <Table.HeaderCell scope="col">{t('column.fromType')}</Table.HeaderCell>
+                                    <Table.HeaderCell scope="col">{t('column.toType')}</Table.HeaderCell>
+                                    {toggleValue === 'custom' &&
+                                        <Table.HeaderCell scope="col">{t('column.fromApplication')}</Table.HeaderCell>}
+                                    {toggleValue === 'custom' &&
+                                        <Table.HeaderCell scope="col">{t('column.toApplication')}</Table.HeaderCell>}
+                                    {toggleValue === 'custom' &&
+                                        <Table.HeaderCell scope="col">{t('column.actions')}</Table.HeaderCell>}
+                                </Table.Row>
+                            </Table.Header>
+                            <Table.Body>
+                                {sortData?.map((value, i) => {
+                                    return (
+                                        <Table.ExpandableRow expandOnRowClick id={"table-row-" + i} key={i}
+                                                             expansionDisabled={toggleValue !== 'custom'}
+                                                             content={toggleValue === 'custom' ?
+                                                                 <ValueConvertingPanel id={i}
+                                                                                       existingValueConverting={value}/> : <></>}>
+                                            <Table.DataCell scope="row">{value.id}</Table.DataCell>
+                                            <Table.DataCell scope="row">{value.displayName}</Table.DataCell>
+                                            <Table.DataCell scope="row">{value.fromTypeId}</Table.DataCell>
+                                            <Table.DataCell scope="row">{value.toTypeId}</Table.DataCell>
+                                            {toggleValue === 'custom' && <Table.DataCell
+                                                scope="row">{getSourceApplicationDisplayNameById(value.fromApplicationId, sourceApplications)}</Table.DataCell>}
+                                            {toggleValue === 'custom' && <Table.DataCell
+                                                scope="row">{getDestinationDisplayName(value.toApplicationId)}</Table.DataCell>}
+                                            {toggleValue === 'custom' &&
+                                                <Table.DataCell scope="row">{actionMenu(value)}</Table.DataCell>}
+                                        </Table.ExpandableRow>
+                                    );
+                                })}
+                            </Table.Body>
+                        </Table>
+                    </Box>
+                    <HStack justify={"center"}>
+                        {rows && rows.length > rowsPerPage &&
+                            <Pagination
+                                page={page}
+                                onPageChange={setPage}
+                                count={Math.ceil(rows.length / rowsPerPage)}
+                                size="small"
+                            />}
+                    </HStack>
+                </VStack>
+                :
+                <Loader size={"large"}/>
+            }
         </Box>
     );
 }
