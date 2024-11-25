2f9780c35fb985f9504306a0e73e41332d3f83c7
jennifer-fintlabs <jennifer@fintlabs.no>
2024-11-25 14:15:41 +0100

    Added scripts to allow local development 🐰

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,25 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
+// /api/intern/arkiv -> fint-flyt-archive-gateway
+// /api/intern/metadata -> fint-flyt-discovery-service
+// /api/intern/konfigurasjoner -> fint-flyt-configuration-service
+// /api/intern/sakstittel -> ikke i bruk lenger (tilhørte data-service som nå er flyttet til archive-gateway)
+// /api/intern/historikk -> fint-flyt-history-service
+// /api/intern/integrasjoner -> fint-flyt-integration-service
+// /api/intern/handlinger/instanser -> fint-flyt-instance-service
+// /api/intern/value-convertings -> fint-flyt-value-converting-service
+// /api/intern/authorization -> fint-flyt-authorization-service
+
 let proxies = [
-    {context: '/api/intern/arkiv', target: 'http://localhost:8085'},
-    {context: '/api/intern/metadata', target: 'http://localhost:8084'},
-    {context: '/api/intern/konfigurasjoner', target: 'http://localhost:8082'},
-    {context: '/api/intern/sakstittel', target: 'http://localhost:8085'},
-    {context: '/api/intern/historikk', target: 'http://localhost:8083'},
-    {context: '/api/intern/integrasjoner', target: 'http://localhost:8090'},
-    {context: '/api/intern/handlinger/instanser', target: 'http://localhost:8081'},
-    {context: '/api/intern/value-convertings', target: 'http://localhost:8094'},
-    {context: '/api/intern/authorization', target: 'http://localhost:8086'}
+    {context: '/api/intern/arkiv', target: 'http://localhost:8085/beta/vlfk-no'},
+    {context: '/api/intern/metadata', target: 'http://localhost:8084/beta/vlfk-no'},
+    {context: '/api/intern/konfigurasjoner', target: 'http://localhost:8082/beta/vlfk-no'},
+    // {context: '/api/intern/sakstittel', target: 'http://localhost:8085'},
+    {context: '/api/intern/historikk', target: 'http://localhost:8083/beta/vlfk-no'},
+    {context: '/api/intern/integrasjoner', target: 'http://localhost:8090/beta/vlfk-no'},
+    {context: '/api/intern/handlinger/instanser', target: 'http://localhost:8081/beta/vlfk-no'},
+    {context: '/api/intern/value-convertings', target: 'http://localhost:8094/beta/vlfk-no'},
+    {context: '/api/intern/authorization', target: 'http://localhost:8086/beta/vlfk-no'}
 ];
 
 module.exports = function (app) {
---
+++ startUpForwards.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+services=(
+  "fint-flyt-archive-gateway"
+  "fint-flyt-discovery-service"
+  "fint-flyt-configuration-service"
+  "fint-flyt-history-service"
+  "fint-flyt-integration-service"
+  "fint-flyt-instance-service"
+  "fint-flyt-value-converting-service"
+  "fint-flyt-authorization-service"
+)
+ports=(
+  "8085:8080"
+  "8084:8080"
+  "8082:8080"
+  "8083:8080"
+  "8090:8080"
+  "8081:8080"
+  "8094:8080"
+  "8086:8080"
+)
+
+NAMESPACE="vlfk-no"
+
+start_port_forwarding() {
+  echo "Starting port-forwarding for services..."
+  for i in "${!services[@]}"; do
+    echo "Starting port-forwarding for ${services[$i]} on ${ports[$i]}"
+
+    output=$(kubectl port-forward service/"${services[$i]}" ${ports[$i]} -n $NAMESPACE > /dev/null 2>&1 & echo $!)
+    if [[ $? -ne 0 ]]; then
+      echo "Error: Failed to start port-forwarding for ${services[$i]} on ${ports[$i]}."
+      echo "Details: $output"
+    else
+      echo "Port-forwarding started for ${services[$i]} on ${ports[$i]}"
+    fi
+  done
+  echo "All port-forwarding processes attempted."
+}
+
+stop_port_forwarding() {
+  echo "Stopping port-forwarding for services..."
+  for service in "${services[@]}"; do
+    echo "Stopping port-forwarding for $service"
+
+    output=$(pkill -f "kubectl port-forward service/$service" 2>&1)
+    if [[ $? -ne 0 ]]; then
+      echo "Error: Failed to stop port-forwarding for $service."
+      echo "Details: $output"
+    else
+      echo "Port-forwarding stopped for $service"
+    fi
+  done
+  echo "All port-forwarding processes stopped (or attempted)."
+}
+
+case "$1" in
+  start)
+    start_port_forwarding
+    ;;
+  stop)
+    stop_port_forwarding
+    ;;
+  *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
