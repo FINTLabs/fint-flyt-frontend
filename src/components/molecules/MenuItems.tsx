d47f01523e172c15d4c87f73bd731e4a645e71f8
IngMyr <ingrid.myrann@gmail.com>
2025-05-22 13:11:16 +0200

    Add logout button and fix internalheader item types

--- src/components/molecules/MenuItems.tsx
+++ src/components/molecules/MenuItems.tsx
@@ -1,43 +1,58 @@
-import { useNavigate } from "react-router-dom";
-import routes from "../../routes/Routes";
-import {useTranslation} from "react-i18next";
-import {Button} from "@navikt/ds-react";
-import {useContext} from "react";
-import {AuthorizationContext} from "../../context/AuthorizationContext";
+import { useNavigate } from 'react-router-dom';
+import routes from '../../routes/Routes';
+import { useTranslation } from 'react-i18next';
+import { Button, InternalHeader } from '@navikt/ds-react';
+import { useContext } from 'react';
+import { AuthorizationContext } from '../../context/AuthorizationContext';
 
 const MenuItems = () => {
-    const {t} = useTranslation("translations", {keyPrefix: "menuItems"});
-    const {hasAccessToUserPermissionPage} = useContext(AuthorizationContext)
+    const { t } = useTranslation('translations', { keyPrefix: 'menuItems' });
+    const { hasAccessToUserPermissionPage } = useContext(AuthorizationContext);
     const history = useNavigate();
     return (
         <>
             {routes
                 .filter((route) => route.inNavigationMenu)
                 .map((route) => (
-                    <Button
-                        size={"medium"}
+                    <InternalHeader.Button
+                        key={route.name}
+                        onClick={() => {
+                            history(route.path);
+                        }}>
+                        {t(route.name)}
+                    </InternalHeader.Button>
+/*                    <Button
+                        size={'medium'}
                         // style={{color: "white"}}
-                        variant={"tertiary-neutral"}
+                        variant={'tertiary-neutral'}
                         onClick={() => {
                             history(route.path);
                         }}
-                        key={route.name}
-                    >
+                        key={route.name}>
                         {t(route.name)}
-                    </Button>
+                    </Button>*/
                 ))}
-            {hasAccessToUserPermissionPage &&
-                <Button
-                    size={"medium"}
-                    // style={{color: "white"}}
-                    variant={"tertiary-neutral"}
-                    onClick={() => {
-                        history('/useraccess');
-                    }}
-                    key={'useraccess'}
-                >
-                    {t('useraccess')}
-                </Button>
+            {
+                hasAccessToUserPermissionPage && (
+                    <InternalHeader.Button
+                        key={'useraccess'}
+                        onClick={() => {
+                            history('/useraccess');
+                        }}>
+                        {t('useraccess')}
+                    </InternalHeader.Button>
+                )
+                /*                <Button
+                                    size={"medium"}
+                                    // style={{color: "white"}}
+                                    variant={"tertiary-neutral"}
+                                    onClick={() => {
+                                        history('/useraccess');
+                                    }}
+                                    key={'useraccess'}
+                                >
+                                    {t('useraccess')}
+                                </Button>*/
             }
         </>
     );
