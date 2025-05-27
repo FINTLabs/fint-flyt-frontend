ba0cee241ce5bcc63fa28773ffcd1068cc4615eb
IngMyr <ingrid.myrann@gmail.com>
2025-05-27 11:40:25 +0200

    Cleanup of deprecated use of List component

--- src/components/pages/Version.tsx
+++ src/components/pages/Version.tsx
@@ -1,50 +1,52 @@
-import React, {useContext, useEffect} from 'react';
-import {BodyLong, Box, List, VStack} from "@navikt/ds-react";
-import PageTemplate from "../templates/PageTemplate";
-import {getAboutByLanguage, getVersionDataByLanguage,} from "../utils/version/VersionTexts";
-import {RouteComponent} from "../../routes/Route";
-import {useTranslation} from "react-i18next";
-import {AuthorizationContext} from "../../context/AuthorizationContext";
-import {useNavigate} from "react-router-dom";
+import React, { useContext, useEffect } from 'react';
+import { BodyLong, Box, Heading, List, VStack } from '@navikt/ds-react';
+import PageTemplate from '../templates/PageTemplate';
+import { getAboutByLanguage, getVersionDataByLanguage } from '../utils/version/VersionTexts';
+import { RouteComponent } from '../../routes/Route';
+import { useTranslation } from 'react-i18next';
+import { AuthorizationContext } from '../../context/AuthorizationContext';
+import { useNavigate } from 'react-router-dom';
 
 const Version: RouteComponent = () => {
-    const {i18n} = useTranslation();
-    const { authorized, getAuthorization} = useContext(AuthorizationContext)
+    const { i18n } = useTranslation();
+    const { authorized, getAuthorization } = useContext(AuthorizationContext);
     const history = useNavigate();
 
-    if(!authorized) {
-        history('/forbidden')
+    if (!authorized) {
+        history('/forbidden');
     }
     useEffect(() => {
-        getAuthorization()
+        getAuthorization();
     }, []);
 
     return (
         <PageTemplate id={'version'} keyPrefix={'pages.version'}>
             <Box
-                id={"version-information"}
-                background={"surface-default"}
+                id={'version-information'}
+                background={'surface-default'}
                 padding="6"
-                borderRadius={"large"}
+                borderRadius={'large'}
                 borderWidth="2"
-                borderColor={"border-subtle"}
-            >
-                <VStack gap={"6"}>
+                borderColor={'border-subtle'}>
+                <VStack gap={'6'}>
                     <BodyLong>{getAboutByLanguage(i18n.language)}</BodyLong>
-                    {/*<BodyLong>*/}
-                        <VStack gap={"6"}>
-                            {(getVersionDataByLanguage(i18n.language)).map((value, i) => (
-                                <List key={i} as="ul" title={value.heading}>
-                                    {value.updates.map((update, i) => (
-                                        <List.Item key={i}>{update}</List.Item>
+                    <VStack gap={'6'}>
+                        {getVersionDataByLanguage(i18n.language).map((value, i) => (
+                            <VStack key={i} gap={'0'}>
+                                <Heading level="2" size="small" spacing={false}>
+                                    {value.heading}
+                                </Heading>
+                                <List as="ul">
+                                    {value.updates.map((update, liIndex) => (
+                                        <List.Item key={liIndex}>{update}</List.Item>
                                     ))}
                                 </List>
-                            ))}
-                        </VStack>
-                    {/*</BodyLong>*/}
+                            </VStack>
+                        ))}
+                    </VStack>
                 </VStack>
             </Box>
         </PageTemplate>
-    )
-}
+    );
+};
 export default Version;
