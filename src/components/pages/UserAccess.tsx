36d7a381c0068e4284562805f6640f0b5a8008a9
IngMyr <ingrid.myrann@gmail.com>
2025-11-19 14:31:35 +0100

    FFS-1489 Fix checbox bug on save and cleanup page

--- src/components/pages/UserAccess.tsx
+++ src/components/pages/UserAccess.tsx
@@ -1,23 +1,35 @@
-import { RouteComponent } from "../../routes/Route";
-import PageTemplate from "../templates/PageTemplate";
-import * as React from "react";
-import { useContext, useEffect, useState } from "react";
-import { AuthorizationContext } from "../../context/AuthorizationContext";
-import { useNavigate } from "react-router";
-import { Alert, Box, Button, Checkbox, Heading, HStack, Loader, Table, VStack, Pagination, SortState } from "@navikt/ds-react";
-import { useTranslation } from "react-i18next";
-import { PencilWritingIcon } from "@navikt/aksel-icons";
+import { RouteComponent } from '../../routes/Route';
+import PageTemplate from '../templates/PageTemplate';
+import * as React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { AuthorizationContext } from '../../context/AuthorizationContext';
+import { useNavigate } from 'react-router';
+import {
+    Alert,
+    Box,
+    Button,
+    Checkbox,
+    Heading,
+    HStack,
+    Loader,
+    Table,
+    VStack,
+    Pagination,
+    SortState,
+} from '@navikt/ds-react';
+import { useTranslation } from 'react-i18next';
+import { PencilWritingIcon } from '@navikt/aksel-icons';
 import { IAlertMessage, Page } from '../types/TableTypes';
 import useAuthorizationRepository from '../../api/useAuthorizationRepository';
 import { IUser } from '../types/UserTypes';
 
 const UserAccess: RouteComponent = () => {
-    const AuthorizationRepository = useAuthorizationRepository()
+    const AuthorizationRepository = useAuthorizationRepository();
     const { t } = useTranslation('translations', { keyPrefix: 'pages.useraccess' });
     const { hasAccessToUserPermissionPage } = useContext(AuthorizationContext);
     const [error, setError] = useState<IAlertMessage | undefined>(undefined);
     const history = useNavigate();
-    const { authorized} = useContext(AuthorizationContext)
+    const { authorized } = useContext(AuthorizationContext);
 
     useEffect(() => {
         if (authorized === false) {
@@ -30,17 +42,23 @@ const UserAccess: RouteComponent = () => {
     }
 
     const [users, setUsers] = useState<IUser[] | undefined>(undefined);
+    const [initialUsers, setInitialUsers] = useState<IUser[] | undefined>(undefined);
+
     const [editMode, setEditMode] = useState<boolean>(false);
     const [page, setPage] = useState(1);
     const [pageSize, setPageSize] = useState(10);
     const [totalPages, setTotalPages] = useState(0);
-    const [sort, setSort] = useState<SortState | undefined>({ orderBy: 'name', direction: "ascending" });
+    const [sort, setSort] = useState<SortState | undefined>({
+        orderBy: 'name',
+        direction: 'ascending',
+    });
 
     const fetchUsers = () => {
         AuthorizationRepository.getUsers(page - 1, pageSize)
             .then((response) => {
                 const pageableResponse: Page<IUser> = response.data;
                 setUsers(pageableResponse.content);
+                setInitialUsers(pageableResponse.content);
                 setTotalPages(pageableResponse.totalPages ?? 0);
                 setError(undefined);
             })
@@ -68,11 +86,13 @@ const UserAccess: RouteComponent = () => {
     const updateUserAccess = (sub: string, sourceAppInput: number, permissionCheck: boolean) => {
         if (!users) return;
 
-        const updatedUsers = users.map(user => {
+        const updatedUsers = users.map((user) => {
             if (user.objectIdentifier === sub) {
                 const newSourceApplicationIds = permissionCheck
                     ? [...user.sourceApplicationIds, sourceAppInput]
-                    : user.sourceApplicationIds.filter(sourceAppId => sourceAppId !== sourceAppInput);
+                    : user.sourceApplicationIds.filter(
+                          (sourceAppId) => sourceAppId !== sourceAppInput
+                      );
                 return { ...user, sourceApplicationIds: newSourceApplicationIds };
             }
             return user;
@@ -91,111 +111,172 @@ const UserAccess: RouteComponent = () => {
     };
 
     const handleSortChange = (sortKey: string) => {
-        setSort(prevSort => {
-            return prevSort && sortKey === prevSort.orderBy && prevSort.direction === "descending"
+        setSort((prevSort) => {
+            return prevSort && sortKey === prevSort.orderBy && prevSort.direction === 'descending'
                 ? undefined
                 : {
-                    orderBy: sortKey,
-                    direction:
-                        prevSort && sortKey === prevSort.orderBy && prevSort.direction === "ascending"
-                            ? "descending"
-                            : "ascending",
-                };
+                      orderBy: sortKey,
+                      direction:
+                          prevSort &&
+                          sortKey === prevSort.orderBy &&
+                          prevSort.direction === 'ascending'
+                              ? 'descending'
+                              : 'ascending',
+                  };
         });
     };
 
     const selectOptions = [
-        { value: "10", label: "10" },
-        { value: "25", label: "25" },
-        { value: "50", label: "50" },
-        { value: "100", label: "100" }
+        { value: '10', label: '10' },
+        { value: '25', label: '25' },
+        { value: '50', label: '50' },
+        { value: '100', label: '100' },
     ];
 
     return (
         <PageTemplate id={'useraccess'} keyPrefix={'pages.useraccess'} customHeading>
-            <HStack id={'instances-custom-header'} align={"center"} justify={"space-between"} gap={"2"} wrap={false}>
-                <Heading size={"medium"}>{t('header')}</Heading>
+            <HStack
+                id={'instances-custom-header'}
+                align={'center'}
+                justify={'space-between'}
+                gap={'2'}
+                wrap={false}>
+                <Heading size={'medium'}>{t('header')}</Heading>
                 <Button
                     id={'edit-toggle-btn'}
                     disabled={!users || editMode}
-                    onClick={() => setEditMode((prevState => !prevState))}
-                    size={"small"}
-                    icon={<PencilWritingIcon aria-hidden />}
-                >{t('button.edit')}
+                    onClick={() => setEditMode((prevState) => !prevState)}
+                    size={'small'}
+                    icon={<PencilWritingIcon aria-hidden />}>
+                    {t('button.edit')}
                 </Button>
             </HStack>
-            {error && <Alert style={{ maxWidth: '100%' }} variant="error">{error.message}</Alert>}
+            {error && (
+                <Alert style={{ maxWidth: '100%' }} variant="error">
+                    {error.message}
+                </Alert>
+            )}
             <Box background={'surface-default'} style={{ minHeight: '70vh' }}>
-                {users ? <VStack gap={"6"}>
-                        <Table sort={sort} onSortChange={(sortKey) => handleSortChange(sortKey ? sortKey : "name")} id={'useraccess-table'}>
+                {users ? (
+                    <VStack gap={'6'}>
+                        <Table
+                            sort={sort}
+                            onSortChange={(sortKey) => handleSortChange(sortKey ? sortKey : 'name')}
+                            id={'useraccess-table'}>
                             <Table.Header>
                                 <Table.Row id={'table-row-header'}>
-                                    <Table.ColumnHeader
-                                        id={'column-header-name'}>{t('table.column.name')}</Table.ColumnHeader>
-                                    <Table.ColumnHeader
-                                        id={'column-header-email'}>{t('table.column.email')}</Table.ColumnHeader>
-                                    <Table.ColumnHeader id={'column-header-acos'}>ACOS</Table.ColumnHeader>
-                                    <Table.ColumnHeader id={'column-header-egrv'}>eGrunnerverv</Table.ColumnHeader>
-                                    <Table.ColumnHeader id={'column-header-digisak'}>Digisak</Table.ColumnHeader>
-                                    <Table.ColumnHeader id={'column-header-vigo'}>VIGO</Table.ColumnHeader>
-                                    <Table.ColumnHeader id={'column-header-altinn'}>Altinn</Table.ColumnHeader>
-                                    <Table.ColumnHeader id={'column-header-hmsreg'}>HMSReg</Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-name'}>
+                                        {t('table.column.name')}
+                                    </Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-email'}>
+                                        {t('table.column.email')}
+                                    </Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-acos'} align={'center'}>
+                                        ACOS
+                                    </Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-egrv'} align={'center'}>
+                                        eGrunnerverv
+                                    </Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-digisak'} align={'center'}>
+                                        Digisak
+                                    </Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-vigo'} align={'center'}>
+                                        VIGO
+                                    </Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-altinn'} align={'center'}>
+                                        Altinn
+                                    </Table.ColumnHeader>
+                                    <Table.ColumnHeader id={'column-header-hmsreg'} align={'center'}>
+                                        HMSReg
+                                    </Table.ColumnHeader>
                                 </Table.Row>
                             </Table.Header>
                             <Table.Body>
                                 {users?.map((value, i) => {
                                     return (
                                         <Table.Row id={'table-row-' + i} key={i}>
-                                            <Table.DataCell id={'table-row-cell-name-' + i}>{value.name}</Table.DataCell>
-                                            <Table.DataCell id={'table-row-cell-' + i}>{value.email}</Table.DataCell>
-                                            {[1, 2, 3, 4, 5, 6].map(sourceApp => <Table.DataCell
-                                                key={`${value.objectIdentifier}-permission-${sourceApp}`}>
-                                                <Checkbox
-                                                    id={'check-row-' + i + '-cell-' + sourceApp}
-                                                    disabled={!editMode}
-                                                    checked={value.sourceApplicationIds.includes(sourceApp)}
-                                                    onChange={(e) => updateUserAccess(value.objectIdentifier, sourceApp, e.target.checked)}
-                                                    hideLabel
-                                                > {t('giveAccess')}
-                                                </Checkbox>
-                                            </Table.DataCell>)}
+                                            <Table.DataCell id={'table-row-cell-name-' + i}>
+                                                {value.name}
+                                            </Table.DataCell>
+                                            <Table.DataCell id={'table-row-cell-' + i}>
+                                                {value.email}
+                                            </Table.DataCell>
+                                            {[1, 2, 3, 4, 5, 6].map((sourceApp) => (
+                                                <Table.DataCell
+                                                    key={`${value.objectIdentifier}-permission-${sourceApp}`}>
+                                                    <HStack width={'100%'} justify={'center'}>
+                                                        <Checkbox
+                                                            id={'check-row-' + i + '-cell-' + sourceApp}
+                                                            disabled={!editMode}
+                                                            checked={value.sourceApplicationIds.includes(
+                                                                sourceApp
+                                                            )}
+                                                            onChange={(e) =>
+                                                                updateUserAccess(
+                                                                    value.objectIdentifier,
+                                                                    sourceApp,
+                                                                    e.target.checked
+                                                                )
+                                                            }
+                                                            hideLabel>
+                                                            {t('giveAccess')}
+                                                        </Checkbox>
+                                                    </HStack>
+                                                </Table.DataCell>
+                                            ))}
                                         </Table.Row>
                                     );
                                 })}
                             </Table.Body>
                         </Table>
-                        {editMode &&
-                            <HStack justify={"end"} gap={"6"} style={{ marginRight: '24px' }}>
+                        {editMode && (
+                            <HStack justify={'end'} gap={'6'} style={{ marginRight: '24px' }}>
                                 <Button id="form-save-btn" type="submit" onClick={updateUsers}>
                                     {t('button.save')}
                                 </Button>
-                                <Button id="form-cancel-btn"
-                                        variant={"secondary"}
-                                        onClick={() => {
-                                    setUsers(users);
-                                    setEditMode(false);
-                                }}>
+                                <Button
+                                    id="form-cancel-btn"
+                                    variant={'secondary'}
+                                    onClick={() => {
+                                        setUsers(initialUsers);
+                                        setEditMode(false);
+                                    }}>
                                     {t('button.cancel')}
                                 </Button>
-                            </HStack>}
-                        <HStack justify={"center"} align={"center"} style={{ marginTop: '16px' }}>
-                            <Box>
-                                <label htmlFor="select-row-count">{t('numberPerPage')}</label>
-                                <select id="select-row-count" value={pageSize} onChange={handlePageSizeChange}>
-                                    {selectOptions.map(option => (
-                                        <option key={option.value} value={option.value}>{option.label}</option>
-                                    ))}
-                                </select>
-                            </Box>
-                            <Pagination
-                                page={page}
-                                onPageChange={handlePageChange}
-                                count={totalPages}
-                                size="small"
-                            />
-                        </HStack>
+                            </HStack>
+                        )}
+                        {!editMode && (
+                            <HStack
+                                justify={'center'}
+                                align={'center'}
+                                style={{ marginTop: '16px' }}>
+                                <HStack gap={'2'} align={'center'}>
+                                    <label htmlFor="select-row-count">{t('numberPerPage')}</label>
+                                    <select
+                                        id="select-row-count"
+                                        value={pageSize}
+                                        onChange={handlePageSizeChange}>
+                                        {selectOptions.map((option) => (
+                                            <option key={option.value} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </HStack>
+                                {totalPages > 1 && (
+                                    <Pagination
+                                        page={page}
+                                        onPageChange={handlePageChange}
+                                        count={totalPages}
+                                        size="small"
+                                    />
+                                )}
+                            </HStack>
+                        )}
                     </VStack>
-                    : <Loader />}
+                ) : (
+                    <Loader />
+                )}
             </Box>
         </PageTemplate>
     );
