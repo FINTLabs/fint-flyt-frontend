634363c68a19774bb54a19bce8ac102c74ae46da
IngMyr <ingrid.myrann@gmail.com>
2026-09-08 09:00:57 +0200

    Update VC helpertexts

--- src/pages/ValueConvertingPage.tsx
+++ src/pages/ValueConvertingPage.tsx
@@ -2,13 +2,14 @@ import ValueConverting from '../features/valueConverting/ValueConverting';
 import { RouteComponent } from '../routes/Route';
 import PageTemplate from '../shared/components/layout/PageTemplate';
 
-const ValueConvertingPage: RouteComponent = () => 
-<PageTemplate
-    id={'valueConverting'}
-    keyPrefix={'pages.valueConverting'}
-    headingHelpTextKey="help.header"
->
-    <ValueConverting />
-</PageTemplate>
+const ValueConvertingPage: RouteComponent = () => (
+    <PageTemplate
+        id={'valueConverting'}
+        keyPrefix={'pages.valueConverting'}
+        headingHelpTextKey="help.valueConverting"
+    >
+        <ValueConverting />
+    </PageTemplate>
+);
 
 export default ValueConvertingPage;
